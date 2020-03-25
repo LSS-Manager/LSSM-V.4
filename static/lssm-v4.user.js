71a38b94e6626bf4b220c0cecd05fb79c13d24fe
jan <jan.boehmerle@ledbrain.de>
2020-03-25 17:01:04 +0100

    🌐🇷🇴 basic ro_RO support.

--- static/lssm-v4.user.js
+++ static/lssm-v4.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         LSS-Manager V.4 - Dev Version
-// @version      4.0.1+DE-NL-US-UK-AU-ES-PL-SE-IT-FR-RU-DK-NO-CZ-TR-PT-BR-MX-UA-JP
+// @version      4.0.1+DE-NL-US-UK-AU-ES-PL-SE-IT-FR-RU-DK-NO-CZ-TR-PT-BR-MX-UA-JP-RO
 // @description  Das Tool für das LSS in der Version 4
-// @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|meldkamerspel\.com|missionchief\.com|missionchief\.co.uk|missionchief-australia\.com|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|operador112\.pt|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/.*$/
+// @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|meldkamerspel\.com|missionchief\.com|missionchief\.co.uk|missionchief-australia\.com|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|operador112\.pt|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|jocdispecerat112\.com)\/.*$/
 // @homepage     https://v4.lssm.ledbrain.de/
 // @updateURL    https://v4.lssm.ledbrain.de/lssm-v4.user.js
 // @grant        none
