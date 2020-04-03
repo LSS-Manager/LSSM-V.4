69cfa252d1b5f473a4d66e81995dce91747b2a49
jan <jan.boehmerle@ledbrain.de>
2020-04-03 21:34:32 +0200

    ⚗️ [renameFz] empty container in dispatch and stations

--- src/modules/renameFz/main.js
+++ src/modules/renameFz/main.js
@@ -1,8 +1,30 @@
 import renameFz from './renameFz.vue';
 
-const vehicleTable = document.querySelector('#vehicle_table');
-new window.lssmv4.Vue({
-    store: window.lssmv4.$store,
-    i18n: window.lssmv4.$i18n,
-    render: h => h(renameFz),
-}).$mount(vehicleTable);
+const mount = () => {
+    const vehicleTable = document.querySelector('#vehicle_table');
+    const clear = document.createElement('div');
+    clear.classList.add('clear');
+    vehicleTable.parentNode.insertBefore(clear, vehicleTable);
+    new window.lssmv4.Vue({
+        store: window.lssmv4.$store,
+        i18n: window.lssmv4.$i18n,
+        render: h => h(renameFz),
+    }).$mount(clear);
+};
+
+if (!document.querySelector('img.online_icon')) {
+    if (document.getElementById('tab_vehicle')) {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(record => {
+                Array.from(record.addedNodes).find(
+                    node => node.tagName === 'SCRIPT'
+                ) && mount();
+            });
+        });
+        observer.observe(document.querySelector('#tab_vehicle'), {
+            childList: true,
+        });
+    } else {
+        mount();
+    }
+}
--- src/modules/renameFz/renameFz.vue
+++ src/modules/renameFz/renameFz.vue
@@ -5,12 +5,25 @@
             :class="[`glyphicon-eye-${isHidden ? 'open' : 'close'}`]"
             @click="isHidden = !isHidden"
         ></span>
+        <div v-show="!isHidden">
+            <p>
+                <strong>{{ $t('modules.renameFz.name') }}</strong>
+            </p>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'renameFz',
+    data() {
+        return {
+            isHidden: true,
+            templates: {
+                test1: [],
+            },
+        };
+    },
 };
 </script>
 
