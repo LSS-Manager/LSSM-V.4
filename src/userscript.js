b15f578deae4c8fce5a91368617655576151282a
jan <jan.boehmerle@ledbrain.de>
2020-05-04 19:11:53 +0200

    ♻️ [userscript] improved script

--- src/userscript.js
+++ src/userscript.js
@@ -2,7 +2,7 @@ if (typeof user_id !== void 0 && typeof I18n !== void 0) {
     const script = document.createElement('script');
     // eslint-disable-next-line no-undef
     script.src = `${host}${I18n.locale}/core.js?_=${new Date().getTime()}&uid=${
-        I18n.locale
+      I18n.locale
     }-${user_id}`;
     script.setAttribute('type', 'module');
     script.setAttribute('async', '');
