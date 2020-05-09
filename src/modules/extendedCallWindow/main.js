4f54b34fd7d30f41995dfd3a632c0b5f0abaf219
jan <jan.boehmerle@ledbrain.de>
2020-05-09 15:26:15 +0200

    🐛 [extendedCallWindow] no execution on 404

--- src/modules/extendedCallWindow/main.js
+++ src/modules/extendedCallWindow/main.js
@@ -1,18 +1,24 @@
 import moment from 'moment';
 
-moment.locale(BUILD_LANG);
+(() => {
+    if (document.querySelector('.missionNotFound')) return;
 
-const generationDate = moment(
+    moment.locale(BUILD_LANG);
+
+    const generationDate = moment(
+        document
+            .querySelector('#missionH1')
+            .getAttribute('data-original-title')
+            .replace(/^.*?:/, '')
+            .trim(),
+        window.lssmv4.$t(
+            'modules.extendedCallWindow.generationDate.inputFormat'
+        )
+    );
+
+    const generationDateNode = document.createElement('i');
+    generationDateNode.innerText = `[${generationDate.fromNow()} (${generationDate.calendar()})]`;
     document
-        .querySelector('#missionH1')
-        .getAttribute('data-original-title')
-        .replace(/^.*?:/, '')
-        .trim(),
-    window.lssmv4.$t('modules.extendedCallWindow.generationDate.inputFormat')
-);
-
-const generationDateNode = document.createElement('i');
-generationDateNode.innerText = `[${generationDate.fromNow()} (${generationDate.calendar()})]`;
-document
-    .querySelector('#mission_general_info small')
-    .appendChild(generationDateNode);
+        .querySelector('#mission_general_info small')
+        .appendChild(generationDateNode);
+})();
