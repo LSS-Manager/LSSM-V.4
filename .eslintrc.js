5825fdbed4186ce49d806cdcee665140a0e6fd71
jan <jan.boehmerle@ledbrain.de>
2020-05-15 18:15:04 +0200

    ⬆️🚨 [build] ESLint Babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,31 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
     },
-    'plugins': [
-        'vue',
-        'prettier'
-    ],
-    'extends': ['eslint:recommended', 'plugin:vue/essential'],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly',
-        'process': 'readonly',
-        '__dirname': 'readonly',
-        'VERSION': 'readonly',
-        'BUILD_LANG': 'readonly',
-        'MODULE_ID': 'readonly',
-        'MODE': 'readonly',
-        'FALLBACK_LOCALES': 'readonly',
+    plugins: ['vue', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:vue/essential'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        VERSION: 'readonly',
+        BUILD_LANG: 'readonly',
+        MODULE_ID: 'readonly',
+        MODE: 'readonly',
+        FALLBACK_LOCALES: 'readonly',
     },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-    'rules': {
+    rules: {
         'guard-for-in': 2,
         'no-prototype-builtins': 0,
         'prettier/prettier': 'error',
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+        'semi': ['error', 'always'],
+    },
 };
