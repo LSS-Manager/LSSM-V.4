9613d49de757e1bf2f5ede695015a2c0078ee523
jan <jan.boehmerle@ledbrain.de>
2020-07-06 16:44:13 +0200

    ⚡️ [arrCounter] Improving Performance

--- src/modules/extendedCallWindow/assets/arrCounter.ts
+++ src/modules/extendedCallWindow/assets/arrCounter.ts
@@ -2,6 +2,12 @@ export default async (
     LSSM: Vue,
     getSetting: (key: string) => Promise<boolean>
 ): Promise<void> => {
+    const ARRContainer = document.getElementById(
+        'mission-aao-group'
+    ) as HTMLDivElement;
+
+    if (!ARRContainer) return;
+
     const counterClass = LSSM.$store.getters.nodeAttribute('arr-counter');
     const highlightClass = LSSM.$store.getters.nodeAttribute('arr-clicked');
 
@@ -33,14 +39,47 @@ export default async (
 
     const highlight = await getSetting('arrClickHighlight');
 
-    let counterNodes = [] as HTMLSpanElement[];
+    const counterNodes = {} as { [aao_id: string]: HTMLSpanElement };
 
     const resetCounters = () => {
-        counterNodes.forEach(counter => {
+        Object.values(counterNodes).forEach(counter => {
             counter.setAttribute('data-amount', '0x');
             counter.parentElement?.classList.remove(highlightClass);
         });
     };
+
+    ARRContainer.addEventListener('mouseup', e => {
+        const targetARR = (e.target as HTMLElement)?.closest(
+            '.aao, .vehicle_group'
+        );
+
+        if (
+            !targetARR ||
+            !ARRContainer.contains(targetARR) ||
+            targetARR.querySelector('.label-danger')
+        )
+            return;
+
+        const arrId =
+            targetARR.getAttribute('aao_id') ||
+            targetARR.getAttribute('vehicle_group_id');
+        if (!arrId) return;
+        const counterNode = counterNodes[arrId];
+
+        if (targetARR.getAttribute('reset') === 'true') resetCounters();
+
+        counterNode.setAttribute(
+            'data-amount',
+            parseInt(
+                counterNode.getAttribute('data-amount')?.replace(/\D/g, '') ||
+                    '0'
+            ) +
+                1 +
+                'x'
+        );
+
+        if (highlight) targetARR.classList.add(highlightClass);
+    });
 
     Array.from(
         document.querySelectorAll('.aao .label, .vehicle_group .label')
@@ -49,29 +88,11 @@ export default async (
         counterNode.classList.add(counterClass);
         counterNode.setAttribute('data-amount', '0x');
         label.insertAdjacentElement('afterend', counterNode);
-        counterNode.parentElement?.addEventListener('click', e => {
-            let arr = e.target as HTMLLinkElement;
-            while (!arr.contains(counterNode) || arr === counterNode) {
-                const parent = arr.parentElement;
-                if (!parent) break;
-                arr = parent as HTMLLinkElement;
-            }
-            if (arr.getAttribute('reset') === 'true') resetCounters();
-            counterNode.setAttribute(
-                'data-amount',
-                parseInt(
-                    counterNode
-                        .getAttribute('data-amount')
-                        ?.replace(/\D/g, '') || '0'
-                ) +
-                    1 +
-                    'x'
-            );
-            if (highlight) arr.classList.add(highlightClass);
-        });
-    });
 
-    counterNodes = Array.from(document.querySelectorAll(`.${counterClass}`));
+        const arrId = label.id.match(/\d+$/)?.[0];
+
+        if (arrId) counterNodes[arrId] = counterNode;
+    });
 
     let resetBtnHolder = document.querySelector(
         '#container_navbar_alarm .navbar-right'
