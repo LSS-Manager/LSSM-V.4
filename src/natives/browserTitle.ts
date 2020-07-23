1e3a9ff4c01400d908c1394408d2670f6a4ed894
jan <jan.boehmerle@ledbrain.de>
2020-07-23 08:47:04 +0200

    ♻️ [browserTitle] improved title

--- src/natives/browserTitle.ts
+++ src/natives/browserTitle.ts
@@ -1,32 +1,29 @@
 export default (LSSM: Vue): void => {
-    const store = LSSM.$store;
-
     const heading = document.querySelector('h1, h2, h3, h4, h5, h6');
-    let title = '';
-    if (heading) {
-        title =
-            heading.textContent
-                ?.trim()
-                .replace(/\n/g, ' ')
-                .replace(/ {2,}/g, ' ') || '';
-        const navbarBrand = document.querySelector('.navbar-brand');
-        if (navbarBrand && navbarBrand?.textContent?.trim())
-            title = `${navbarBrand?.textContent.trim()}: ${title}`;
-        if (window.location !== window.parent.location) title = `[${title}] `;
-        else title = `${title} | `;
-    }
+    let title =
+        heading?.textContent
+            ?.trim()
+            .replace(/\n/g, ' ')
+            .replace(/ {2,}/g, ' ') ||
+        window.location.pathname.replace(/^\/|\/$/g, '');
+    const navbarBrand = document.querySelector('.navbar-brand');
+    if (navbarBrand && navbarBrand?.textContent?.trim())
+        title = `${navbarBrand?.textContent.trim()}: ${title}`;
+    if (window.location !== window.parent.location && title.length)
+        title = `[${title}] `;
+    else if (title.length) title = `${title} | `;
     window.tellParent(
-        `document.title = '${title}${
-            store.state.games[store.state.lang].name
+        `if (document.getElementById('lightbox_box')?.style.display !== 'none') document.title = '${title}${
+            LSSM.$store.state.games[LSSM.$store.state.lang].name
         }';`
     );
-    store
+    LSSM.$store
         .dispatch('hook', {
             event: 'lightboxClose',
             post: true,
             callback() {
                 document.title = `${title}${
-                    store.state.games[store.state.lang].name
+                    LSSM.$store.state.games[LSSM.$store.state.lang].name
                 }`;
             },
         })
