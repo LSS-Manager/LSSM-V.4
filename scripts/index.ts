fef9b0439e123d9c3ff6dd657b19572d3a425ae1
jan <jan.boehmerle@ledbrain.de>
2020-08-14 13:52:51 +0200

    🚧 [scripts] WIP

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,18 +1,23 @@
+import { exec } from 'child_process';
 import sort from './sort';
 
 const scripts = process.argv.splice(2);
 
 const scriptHandlers = {
     sort,
+    lint() {
+        this.sort();
+        // TODO: Does not work yet
+        exec(
+            'eslint ../docs/.vuepress/ ../static/ ../prebuild/ ../build/ ../src/ ../scripts/ --ext .js,.vue -f table --no-error-on-unmatched-pattern --fix'
+        );
+    },
 } as { [key: string]: () => string | void };
 
-const outputs = [] as string[];
-
-scripts.forEach(script => {
-    outputs.push(
+const outputs = scripts.map(
+    script =>
         `### ${script} ###\n\n${scriptHandlers[script]?.() ||
             ''}\n\n=== end ${script} ===`
-    );
-});
+);
 
 console.log(outputs.join('\n\n\n'));
