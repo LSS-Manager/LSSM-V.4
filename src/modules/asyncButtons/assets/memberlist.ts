f297c7a9e8eb04b39e19ed873723b8926544b728
Ron31 <ron3108@gmx.net>
2020-08-18 21:11:02 +0200

    :construction: [asyncButtons] memberlist buttons

--- src/modules/asyncButtons/assets/memberlist.ts
+++ src/modules/asyncButtons/assets/memberlist.ts
@@ -1,24 +1,54 @@
 export default (LSSM: Vue, memberlistSettings: string[]): void => {
-    // Kick Button
-    if (memberlistSettings.includes('kick')) {
-        const kickBtns = Array.from(
-            document.querySelectorAll('td div[id^="rights_"] a:nth-of-type(1)')
-        );
-        kickBtns.forEach(btn => {
-            btn.addEventListener('click', async e => {
-                e.preventDefault();
-                await LSSM.$store
-                    .dispatch('api/request', {
-                        url: btn.getAttribute('href'),
-                    })
-                    .then(({ status }) => {
-                        if (status == 200) {
-                            btn.parentElement?.parentElement?.parentElement?.classList.add(
-                                'hidden'
-                            );
-                        }
-                    });
-            });
+    const table = document.querySelector('table#table');
+    table?.addEventListener('click', async e => {
+        const t = e.target as HTMLElement;
+        if (!t) return;
+        if (t.tagName !== 'a') return;
+        if (
+            t.getAttribute('href') === '#' ||
+            t.getAttribute('href')?.match(/\/verband\/rolecaptionForm\/\d+\/?$/)
+        )
+            return;
+        e.preventDefault();
+        if (
+            t.getAttribute('href')?.match(/\/verband\/kick\/\d+\/?$/) &&
+            !memberlistSettings.includes('kick')
+        )
+            return;
+        if (
+            !t.getAttribute('href')?.match(/\/verband\/kick\/\d+\/?$/) &&
+            !memberlistSettings.includes('ranks')
+        )
+            return;
+
+        Array.from(t.parentElement?.children || []).forEach(childBtn => {
+            childBtn.classList.add('disabled');
         });
-    }
+
+        await LSSM.$store
+            .dispatch('api/request', {
+                url: t.getAttribute('href'),
+            })
+            .then(({ status }) => {
+                if (status !== 200) return;
+                const href = t.getAttribute('href')?.split('/');
+                if (!href) return;
+                if (href[2] === 'kick') {
+                    return t?.parentElement?.parentElement?.parentElement?.classList.add(
+                        'hidden'
+                    );
+                } else {
+                    if (href[4] === '1') {
+                        t.classList.replace('btn-success', 'btn-danger');
+                        href[4] === '1' ? (href[4] = '0') : (href[4] = '1');
+                        t.setAttribute('href', href.join('/'));
+                        Array.from(t.parentElement?.children || []).forEach(
+                            childBtn => {
+                                childBtn.classList.remove('disabled');
+                            }
+                        );
+                    }
+                }
+            });
+    });
 };
