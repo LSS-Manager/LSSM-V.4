1bec78bc9303d87f5b30fbc972ceeb58360f506e
Ron31 <ron3108@gmx.net>
2020-08-19 19:08:45 +0200

    :construction: [asyncButtons] kick dialog

--- src/modules/asyncButtons/assets/memberlist.ts
+++ src/modules/asyncButtons/assets/memberlist.ts
@@ -8,6 +8,70 @@ export default (LSSM: Vue): void => {
         [role: string]: string;
     };
 
+    const storeDispatch = async (
+        roleHolder: HTMLElement,
+        rights: string[],
+        t: HTMLAnchorElement
+    ) => {
+        await LSSM.$store
+            .dispatch('api/request', {
+                url: t.getAttribute('href'),
+            })
+            .then(({ status }) => {
+                if (status !== 200) return;
+                const href = t.getAttribute('href')?.split('/');
+                if (!href) return;
+                if (href[2] === 'kick') {
+                    return t?.parentElement?.parentElement?.parentElement?.classList.add(
+                        'hidden'
+                    );
+                } else {
+                    const set = href[4] === '1';
+                    if (set) {
+                        t.classList.replace('btn-success', 'btn-danger');
+                        href[4] = '0';
+                        t.textContent =
+                            t.textContent?.replace(setText, unsetText) || '';
+                        rights.push(
+                            roles[
+                                t.pathname.match(
+                                    /(?<=\/verband\/)[^/]*/
+                                )?.[0] || ''
+                            ]
+                        );
+                    } else {
+                        t.classList.replace('btn-danger', 'btn-success');
+                        href[4] = '1';
+                        t.textContent =
+                            t.textContent?.replace(unsetText, setText) || '';
+                        rights.splice(
+                            rights.findIndex(
+                                r =>
+                                    r ===
+                                    roles[
+                                        t.pathname.match(
+                                            /(?<=\/verband\/)[^/]*/
+                                        )?.[0] || ''
+                                    ]
+                            ),
+                            1
+                        );
+                    }
+
+                    if (rights.includes(roles['admin']))
+                        roleHolder.textContent = roles['admin'];
+                    else roleHolder.textContent = rights.sort().join(', ');
+
+                    t.setAttribute('href', href.join('/'));
+                    Array.from(t.parentElement?.children || []).forEach(
+                        childBtn => {
+                            childBtn.classList.remove('disabled');
+                        }
+                    );
+                }
+            });
+    };
+
     const setText = LSSM.$t(
         `modules.${MODULE_ID}.memberlistManageUser.set`
     ).toString();
@@ -32,66 +96,35 @@ export default (LSSM: Vue): void => {
             Array.from(t.parentElement?.children || []).forEach(childBtn => {
                 childBtn.classList.add('disabled');
             });
-
-            await LSSM.$store
-                .dispatch('api/request', {
-                    url: t.getAttribute('href'),
-                })
-                .then(({ status }) => {
-                    if (status !== 200) return;
-                    const href = t.getAttribute('href')?.split('/');
-                    if (!href) return;
-                    if (href[2] === 'kick') {
-                        return t?.parentElement?.parentElement?.parentElement?.classList.add(
-                            'hidden'
-                        );
-                    } else {
-                        const set = href[4] === '1';
-                        if (set) {
-                            t.classList.replace('btn-success', 'btn-danger');
-                            href[4] = '0';
-                            t.textContent =
-                                t.textContent?.replace(setText, unsetText) ||
-                                '';
-                            rights.push(
-                                roles[
-                                    t.pathname.match(
-                                        /(?<=\/verband\/)[^/]*/
-                                    )?.[0] || ''
-                                ]
-                            );
-                        } else {
-                            t.classList.replace('btn-danger', 'btn-success');
-                            href[4] = '1';
-                            t.textContent =
-                                t.textContent?.replace(unsetText, setText) ||
-                                '';
-                            rights.splice(
-                                rights.findIndex(
-                                    r =>
-                                        r ===
-                                        roles[
-                                            t.pathname.match(
-                                                /(?<=\/verband\/)[^/]*/
-                                            )?.[0] || ''
-                                        ]
-                                ),
-                                1
-                            );
-                        }
-
-                        if (rights.includes(roles['admin']))
-                            roleHolder.textContent = roles['admin'];
-                        else roleHolder.textContent = rights.sort().join(', ');
-
-                        t.setAttribute('href', href.join('/'));
-                        Array.from(t.parentElement?.children || []).forEach(
-                            childBtn => {
-                                childBtn.classList.remove('disabled');
-                            }
-                        );
-                    }
+            if (t.getAttribute('href')?.split('/')[2] === 'kick') {
+                LSSM.$modal.show('dialog', {
+                    title: LSSM.$t(
+                        `modules.${MODULE_ID}.memberlistManageUser.kickModal.title`
+                    ),
+                    text: LSSM.$t(
+                        `modules.${MODULE_ID}.memberlistManageUser.kickModal.text`
+                    ),
+                    buttons: [
+                        {
+                            title: LSSM.$t(
+                                `modules.${MODULE_ID}.memberlistManageUser.kickModal.btnCancel`
+                            ),
+                            default: true,
+                        },
+                        {
+                            title: LSSM.$t(
+                                `modules.${MODULE_ID}.memberlistManageUser.kickModal.btnConfirm`
+                            ),
+                            handler: () => {
+                                storeDispatch(roleHolder, rights, t);
+                                LSSM.$modal.hide('dialog');
+                            },
+                        },
+                    ],
                 });
+            } else {
+                await storeDispatch(roleHolder, rights, t);
+            }
         });
     });
 };
