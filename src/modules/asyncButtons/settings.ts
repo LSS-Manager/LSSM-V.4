d699fb476f150b929df9d5604918c13f9f6f9104
jan <jan.boehmerle@ledbrain.de>
2020-09-04 20:11:42 +0200

    🐛 [outsource-settings] fix asyncButtons

--- src/modules/asyncButtons/settings.ts
+++ src/modules/asyncButtons/settings.ts
@@ -1,25 +1,23 @@
 export default (): unknown => ({
-    settings: {
-        buildingTax: {
-            type: 'toggle',
-            default: true,
-        },
-        missionPrisoners: {
-            type: 'toggle',
-            default: true,
-        },
-        missionReply: {
-            type: 'toggle',
-            default: false,
-            disabled: () => true,
-        },
-        memberlistManageUser: {
-            type: 'toggle',
-            default: false,
-        },
-        deleteForumPost: {
-            type: 'toggle',
-            default: false,
-        },
+    buildingTax: {
+        type: 'toggle',
+        default: true,
+    },
+    missionPrisoners: {
+        type: 'toggle',
+        default: true,
+    },
+    missionReply: {
+        type: 'toggle',
+        default: false,
+        disabled: () => true,
+    },
+    memberlistManageUser: {
+        type: 'toggle',
+        default: false,
+    },
+    deleteForumPost: {
+        type: 'toggle',
+        default: false,
     },
 });
