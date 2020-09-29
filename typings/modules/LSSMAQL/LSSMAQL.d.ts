53ed75407ccc0c9f870aa36733c14bde9c454682
jan <jan.boehmerle@ledbrain.de>
2020-09-29 11:56:57 +0200

    🔀 rebase lssmaql

--- typings/modules/LSSMAQL/LSSMAQL.d.ts
+++ typings/modules/LSSMAQL/LSSMAQL.d.ts
@@ -1,17 +1,16 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { Token, QueryTree } from 'typings/modules/LSSMAQL/index';
+import { Token } from 'typings/modules/LSSMAQL/index';
 import { APIState } from 'typings/store/api/State';
 
 export interface LSSMAQL {
     faTerminal: IconDefinition;
     query: string;
     token_list: Token[];
-    querytree: QueryTree | null;
+    querytree: unknown[];
 }
 
 export interface LSSMAQLMethods {
     execute(): void;
-    tokenize(): void;
     parse(): void;
 }
 
