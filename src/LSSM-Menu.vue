b1982db5698197822a5ec8a8891d9a6d3065429c
jan <jan.boehmerle@ledbrain.de>
2020-10-15 22:27:39 +0200

    ✨ [👮] support PoliceChief 🥳

--- src/LSSM-Menu.vue
+++ src/LSSM-Menu.vue
@@ -94,8 +94,6 @@ import {
 } from '../typings/LSSM-Menu';
 import { DefaultProps } from 'vue/types/options';
 
-const defaultIconBg = '#C9302C';
-
 export default Vue.extend<
     lssmMenuData,
     lssmMenuMethods,
@@ -108,7 +106,7 @@ export default Vue.extend<
         return {
             id: this.$store.getters.nodeAttribute('indicator'),
             menuId: this.$store.getters.nodeAttribute('indicator_menu'),
-            iconBg: defaultIconBg,
+            iconBg: this.$store.state.policechief ? '#004997' : '#C9302C',
             labelInMenu: false,
             lssmLogo,
             discord: this.$store.state.discord,
@@ -234,7 +232,7 @@ export default Vue.extend<
                     this.$store
                         .dispatch('storage/get', {
                             key: 'iconBG',
-                            defaultValue: defaultIconBg,
+                            defaultValue: this.iconBg,
                         })
                         .then(iconBG => (this.iconBg = iconBG));
             });
