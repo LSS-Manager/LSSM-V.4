fce5105b9af272f9e9f6b8498e2f421965625ab9
Tsumiki <mirkoiz@hotmail.de>
2020-10-16 12:01:30 +0200

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+git config --global push.default matching
+git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git push deploy master
