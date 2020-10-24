3cdc59c5834aa69b8afbc347a95e7d9594fbbe51
Jan <jan.boehmerle@ledbrain.de>
2020-10-24 12:23:40 +0200

    🐛 [push.sh] Fix branch

--- push.sh
+++ push.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
+if [ $TRAVIS_PULL_REQUEST != "false" ]
+then
+    BRANCH=$TRAVIS_BRANCH
+else
+    BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+fi
+
 setup_git() {
   git config --global user.email "travis@lss-manager.de"
   git config --global user.name "LSS-Manager Travis"
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  git checkout -b $BRANCH
   git add -A
   git commit --message "👷 [BUILD] $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/LSS-Manager/LSSM-V.4.git > /dev/null 2>&1
-  if [ $TRAVIS_PULL_REQUEST != "false" ]
-  then
-      git push --quiet --set-upstream origin $TRAVIS_BRANCH
-  else
-      git push --quiet --set-upstream origin $TRAVIS_PULL_REQUEST_BRANCH
-  fi
+  git push --quiet --set-upstream origin $BRANCH
 }
 
 setup_git
