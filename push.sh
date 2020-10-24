5ca8f8f13ef7a089f869b96c9c01b65c936d7b38
Jan <jan.boehmerle@ledbrain.de>
2020-10-24 12:13:05 +0200

    🐛 [push] fix push?

--- push.sh
+++ push.sh
@@ -8,12 +8,17 @@ setup_git() {
 commit_website_files() {
   git checkout -b gh-pages
   git add -A
-  git commit --message "👷 [BUILD] $TRAVIS_BUILD_NUMBER [skip ci]"
+  git commit --message "👷 [BUILD] $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/LSS-Manager/LSSM-V.4.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages
+  git remote add origin https://${GH_TOKEN}@github.com/LSS-Manager/LSSM-V.4.git > /dev/null 2>&1
+  if [ $TRAVIS_PULL_REQUEST != "false" ]
+  then
+      git push --quiet --set-upstream origin $TRAVIS_BRANCH
+  else
+      git push --quiet --set-upstream origin $TRAVIS_PULL_REQUEST_BRANCH
+  fi
 }
 
 setup_git
