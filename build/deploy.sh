d32b9b41d4a6240d2794dd229f242cf383d19831
jan <jan.boehmerle@ledbrain.de>
2020-10-25 15:37:26 +0100

    💚 [deploy] add empty missions dir

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,2 +1,3 @@
 rsync -e "ssh -p $PROD_PORT" -r --delete-after $TRAVIS_BUILD_DIR/dist/ $PROD_USER@$PROD_SERVER:$DIR
 ssh -p $PROD_PORT $PROD_USER@$PROD_SERVER cp -r /var/www/static $DIR
+ssh -p $PROD_PORT $PROD_USER@$PROD_SERVER mkdir $DIR/static/missions
