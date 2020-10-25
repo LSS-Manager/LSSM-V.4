47ed6b40071e3d8dd8c5abeaf299891112fdadb5
Tsumiki-Chan <mirkoiz@hotmail.de>
2020-10-25 13:53:15 +0100

    New Deploy Method V2

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,3 +1,2 @@
 rsync -e "ssh -p $PROD_PORT" -r --delete-after $TRAVIS_BUILD_DIR/dist/ $PROD_USER@$PROD_SERVER:$DIR
-ssh -p $PROD_PORT $PROD_USER@$PROD_SERVER 'cp /var/www/static/.configs.json $DIR/static/.configs.json'
-ssh -p $PROD_PORT $PROD_USER@$PROD_SERVER 'cp /var/www/static/db_access.php $DIR/static/db_access.php'
+ssh -p $PROD_PORT $PROD_USER@$PROD_SERVER 'cp -R /var/www/static $DIR'
