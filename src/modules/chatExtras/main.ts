e3f675b41e15765c83047a39b4189c6e3f471de8
Cr4zyc4k3 <arne.setzer@web.de>
2020-11-04 22:24:42 +0100

    💚 🚨 [ChatExtras] switched hooking method

--- src/modules/chatExtras/main.ts
+++ src/modules/chatExtras/main.ts
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import { AllianceChatMessage } from 'typings/Ingame';
 import { $m, ModuleMainFunction } from 'typings/Module';
 
 export default (async (LSSM, MODULE_ID) => {
@@ -6,28 +7,26 @@ export default (async (LSSM, MODULE_ID) => {
         moduleId: MODULE_ID,
         settingId: 'predefined_style',
     });
-    if (window.location.pathname === '/') {
-        let mission_chat_message_username = document.querySelectorAll(
-            '.mission_chat_message_username'
-        );
-        if (mission_chat_message_username) {
-            mission_chat_message_username.forEach(function (chatMessageHead) {
-                const messageHeader = chatMessageHead as HTMLElement;
-                let rawDate = messageHeader.parentElement?.getAttribute(
-                    'data-message-time'
-                );
-                moment.locale(BUILD_LANG);
-                const timeStampModified = moment(rawDate).format(format);
-                messageHeader.innerText = '[' + timeStampModified + ']';
-                chatMessageHead = messageHeader as HTMLElement;
-            });
-        }
+    let mission_chat_message_username = document.querySelectorAll(
+        '.mission_chat_message_username'
+    );
+    if (mission_chat_message_username) {
+        mission_chat_message_username.forEach(function (chatMessageHead) {
+            const messageHeader = chatMessageHead as HTMLElement;
+            let rawDate = messageHeader.parentElement?.getAttribute(
+                'data-message-time'
+            );
+            moment.locale(BUILD_LANG);
+            const timeStampModified = moment(rawDate).format(format);
+            messageHeader.innerText = '[' + timeStampModified + ']';
+            chatMessageHead = messageHeader as HTMLElement;
+        });
     }
-    await LSSM.$store.dispatch('hook', {
+    await LSSM.$store.dispatch('premodifyParams', {
         event: 'allianceChat',
         callback({}: AllianceChatMessage) {
             moment.locale(BUILD_LANG);
-            date_hidden = moment(n).format(format);
+            e.date_hidden = moment(n).format(format);
         },
     });
 }) as ModuleMainFunction;
