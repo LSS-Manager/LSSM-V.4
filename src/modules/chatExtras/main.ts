e70972c005611a727d9a70d88ded268405a94b54
jan <jan.boehmerle@ledbrain.de>
2020-11-05 19:13:58 +0100

    🚨 [chatExtras] linting & ES6

--- src/modules/chatExtras/main.ts
+++ src/modules/chatExtras/main.ts
@@ -1,34 +1,27 @@
 import moment from 'moment';
 import { AllianceChatMessage } from 'typings/Ingame';
-import { $m, ModuleMainFunction } from 'typings/Module';
+import { ModuleMainFunction } from 'typings/Module';
 
 export default (async (LSSM, MODULE_ID) => {
     const format = await LSSM.$store.dispatch('settings/getSetting', {
         moduleId: MODULE_ID,
         settingId: 'format',
     });
-    let mission_chat_message_username = document.querySelectorAll(
-        '.mission_chat_message_username'
-    );
-    if (mission_chat_message_username) {
-        mission_chat_message_username.forEach(function (chatMessageHead) {
-            const messageHeader = chatMessageHead as HTMLElement;
-            let rawDate = messageHeader.parentElement?.getAttribute(
+    moment.locale(BUILD_LANG);
+    document
+        .querySelectorAll<HTMLSpanElement>('.mission_chat_message_username')
+        .forEach(function(msg_user_span) {
+            const rawDate = msg_user_span.parentElement?.getAttribute(
                 'data-message-time'
             );
-            moment.locale(BUILD_LANG);
             const timeStampModified = moment(rawDate).format(format);
-            if (messageHeader.firstChild != null) {
-                messageHeader.firstChild.textContent =
-                    '[' + timeStampModified + ']';
-                chatMessageHead = messageHeader as HTMLElement;
+            if (msg_user_span.firstChild) {
+                msg_user_span.firstChild.textContent = `[${timeStampModified}]`;
             }
         });
-    }
     await LSSM.$store.dispatch('premodifyParams', {
         event: 'allianceChat',
         callback(e: AllianceChatMessage) {
-            moment.locale(BUILD_LANG);
             e.date = moment(e.iso_timestamp).format(format);
         },
     });
