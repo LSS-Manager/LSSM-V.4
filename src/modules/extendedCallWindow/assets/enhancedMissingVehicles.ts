5d74faa328a802106d5055f5b398f7e3dca3f721
Jan <jan.boehmerle@ledbrain.de>
2020-11-25 13:37:24 +0100

    🐛 [emv] another regex fix

--- src/modules/extendedCallWindow/assets/enhancedMissingVehicles.ts
+++ src/modules/extendedCallWindow/assets/enhancedMissingVehicles.ts
@@ -17,7 +17,7 @@ export default (LSSM: Vue, $m: $m): void => {
     if (!missingRequirementMatches) return;
     const missingRequirements = missingRequirementMatches.map(req => {
         req = req.trim();
-        const isColonMode = !!req.match(/^[\w ]*?: \d+$/);
+        const isColonMode = !!req.match(/^.*: \d+$/);
         return {
             missing: parseInt(
                 req.match(isColonMode ? /\d+$/ : /^\d+/)?.[0] || '0'
