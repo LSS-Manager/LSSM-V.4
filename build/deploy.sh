f888d1e5ba0d98d78c3ab977142a5c2fc0800f44
Tsumiki <mirkoiz@hotmail.de>
2020-12-03 15:58:20 +0100

    :green_heart: Fix Deployment

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
 # Deploy data using rsync
-rsync -e "ssh -o LogLevel=error -o StrictHostKeyChecking=no -p $SERVER_PORT" -r --delete-after $WORK_DIR $SERVER_USER@$SERVER:$DEPLOY_DIR
+rsync -e "ssh -o LogLevel=error -o StrictHostKeyChecking=no -p $SERVER_PORT" -r --delete-after $WORK_DIR/dist $SERVER_USER@$SERVER:$DEPLOY_DIR
 # Copy static configuration
 ssh -o LogLevel=error -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USER@$SERVER "cp -r $DIR_STATIC $DEPLOY_DIR"
