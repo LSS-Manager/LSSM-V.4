4f217fb44a08d418451a8e45665b5c10df50cc93
Tsumiki <mirkoiz@hotmail.de>
2020-12-04 10:30:26 +0100

    :construction_worker: Get Package.version from work path

--- build/get_version.sh
+++ build/get_version.sh
@@ -1,1 +1,1 @@
-PACKAGE_VERSION=$(cat package.json | grep 'version' | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+PACKAGE_VERSION=$(cat $WORK_DIR/package.json | grep 'version' | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
