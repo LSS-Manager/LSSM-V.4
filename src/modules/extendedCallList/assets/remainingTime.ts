e8ca0a0c5c673ba01ef19a0222a9898a94f003fa
jan <jan.boehmerle@ledbrain.de>
2020-12-10 16:51:58 +0100

    ♻️ remove console.log…

--- src/modules/extendedCallList/assets/remainingTime.ts
+++ src/modules/extendedCallList/assets/remainingTime.ts
@@ -4,7 +4,6 @@ export default (LSSM: Vue, greenOnly: boolean): void => {
     LSSM.$store.dispatch('hook', {
         event: 'missionTimer',
         callback({ date_end_calc, id, vehicle_state }: MissionTimer) {
-            console.log(greenOnly, vehicle_state);
             if (greenOnly && vehicle_state !== 2) return;
             const timeElement = document.getElementById(
                 `mission_overview_countdown_${id}`
