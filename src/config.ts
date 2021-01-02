2accca23dcbccba8ceaef56b457d8434ee08993e
Tobias <Suchty112@users.noreply.github.com>
2021-01-03 00:42:09 +0100

    🚑fix serverurl

--- src/config.ts
+++ src/config.ts
@@ -182,5 +182,5 @@ export default {
         'core-modules': ['telemetry', 'releasenotes', 'support'],
     },
     prefix: 'lssmv4',
-    server: 'https://jxn.v4.lssm.ledbrain.de/',
+    server: 'https://proxy.lss-manager.de/v4/',
 } as Config;
