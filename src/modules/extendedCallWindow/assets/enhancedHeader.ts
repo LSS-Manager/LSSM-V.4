7477d2c2aa85efc014a46286d9255fa96b1b16b3
Ron31 <ron3108@gmx.net>
2021-01-06 21:18:30 +0100

    🐛 [ecw] vehicle loading in header without personal numbers

--- src/modules/extendedCallWindow/assets/enhancedHeader.ts
+++ src/modules/extendedCallWindow/assets/enhancedHeader.ts
@@ -28,26 +28,34 @@ export default (sticky: boolean, load: boolean): void => {
     ) as HTMLAnchorElement | null;
     if (load && loadBtn) {
         const people_amount = document.getElementById('amount_of_people');
-        if (!people_amount || !people_amount.parentElement) return;
-        people_amount.parentElement.classList.add(
-            `col-md-${
-                head.querySelector('.pull-right [id^="mission_countdown_"]')
-                    ? 5
-                    : 7
-            }`
-        );
+        if (people_amount && people_amount.parentElement) {
+            people_amount.parentElement.classList.add(
+                `col-md-${
+                    head.querySelector('.pull-right [id^="mission_countdown_"]')
+                        ? 5
+                        : 7
+                }`
+            );
+        }
         const wrapper = document.createElement('div');
         wrapper.classList.add('row');
         const btnWrapper = document.createElement('div');
-        btnWrapper.classList.add('col-md-5');
+        if (people_amount && people_amount.parentElement)
+            btnWrapper.classList.add('col-md-5');
         const clonedBtn = loadBtn.cloneNode(true) as HTMLAnchorElement;
         clonedBtn.onclick = e => {
             e.preventDefault();
             loadBtn.click();
         };
         btnWrapper.append(clonedBtn);
-        people_amount.parentElement.before(wrapper);
-        wrapper.append(people_amount.parentElement);
+        if (people_amount && people_amount.parentElement) {
+            people_amount.parentElement.before(wrapper);
+            wrapper.append(people_amount.parentElement);
+        } else {
+            document
+                .querySelector('#mission_progress_info small')
+                ?.before(wrapper);
+        }
         wrapper.append(btnWrapper);
     }
 };
