7e3d9eb14d7507d4684f9be11eac71aa7bfb1afc
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2021-01-14 11:08:38 +0100

    🎨 removed debug code

--- src/modules/generalExtensions/assets/extensionCloseCall.ts
+++ src/modules/generalExtensions/assets/extensionCloseCall.ts
@@ -1,27 +1,10 @@
 export default (): void => {
-    /* const observer = new MutationObserver(handleObserved);
-    const observertarget = document.getElementById('radio_messages_important');
-    const observerOptions = {
-        childList: true,
-    }; */
     const ListenerTarget = document.getElementById('radio_messages_important');
     if (ListenerTarget == null || window.location.pathname != '/') {
         return;
     }
     ListenerTarget.addEventListener('click', handleListen);
-    /* function handleObserved() {
-        const msgClose = document.getElementsByClassName('radio_message_close');
-        for (let i = 0; i < msgClose.length; i++) {
-            let vid = msgClose[i].getAttribute('vehicle_id');
-            let prev = msgClose[i].previousElementSibling;
-            if (prev === null || vid === null) {
-                return;
-            }
-            prev.classList.add('radio_message_close');
-            prev.setAttribute('vehicle_id', vid);
-        }
-    } */
-    function handleListen(event: { target: any; }) {
+    function handleListen(event: { target: any }) {
         const clickedBtn = event.target;
 
         if (clickedBtn.nextElementSibling.hasAttribute('vehicle_id')) {
