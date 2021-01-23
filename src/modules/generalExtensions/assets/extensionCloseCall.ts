93592cc2543ede56e2bff5d0169f83e0c1aaa918
jan <jan.boehmerle@ledbrain.de>
2021-01-23 18:44:00 +0100

    🚨 [ge/ecc] fix lint warning

--- src/modules/generalExtensions/assets/extensionCloseCall.ts
+++ src/modules/generalExtensions/assets/extensionCloseCall.ts
@@ -1,16 +1,18 @@
 export default (): void => {
     const listenerTarget = document.getElementById('radio_messages_important');
     if (!listenerTarget) return;
-    listenerTarget.addEventListener('click', handleListen);
-    function handleListen(event: { target: any }) {
-        const clickedBtn = event.target;
+    listenerTarget.addEventListener('click', event => {
+        const clickedBtn = event.target as HTMLElement | null;
 
-        if (clickedBtn.nextElementSibling.hasAttribute('vehicle_id')) {
+        if (
+            clickedBtn &&
+            clickedBtn.nextElementSibling?.hasAttribute('vehicle_id')
+        ) {
             clickedBtn.classList.add('radio_message_close');
             clickedBtn.setAttribute(
                 'vehicle_id',
-                clickedBtn.nextElementSibling.getAttribute('vehicle_id')
+                clickedBtn.nextElementSibling?.getAttribute('vehicle_id') ?? '0'
             );
         }
-    }
+    });
 };
