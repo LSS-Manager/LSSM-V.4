38b9648376d7b424df1996c9079de24c621f15ca
jan <jan.boehmerle@ledbrain.de>
2021-02-02 20:31:34 +0100

    🏗️ core outsource stuff of mainpage

--- src/core.ts
+++ src/core.ts
@@ -5,15 +5,10 @@ import * as Tabs from 'vue-slim-tabs';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Notifications from 'vue-notification';
 import LSSMV4 from './LSSMV4.vue';
-import LSSMMenu from './LSSM-Menu.vue';
 import store from './store';
 import i18n from './i18n';
 import utils from './utils';
-import telemetry from './modules/telemetry/main';
-import { BuildingMarkerAdd, RadioMessage } from '../typings/Ingame';
 import { ModuleMainFunction, ModuleSettingFunction } from 'typings/Module';
-import { Color, Toggle } from 'typings/Setting';
-import { Building } from 'typings/Building';
 
 require('./natives/navTabsClicker');
 require('./natives/lightbox');
@@ -50,72 +45,6 @@ require('./natives/lightbox');
 
     window[PREFIX] = LSSM;
 
-    if (window.location.pathname === '/') {
-        window.console.info(
-            `Running %cLSSM%c in Version %c${VERSION}%c`,
-            'font-weight: bold;',
-            'font-weight: normal;',
-            'font-style: italic;'
-        );
-
-        const indicatorWrapper = document.createElement('li') as HTMLLIElement;
-        document
-            .querySelector('.navbar-default .navbar-right')
-            ?.appendChild(indicatorWrapper);
-
-        LSSM.$store
-            .dispatch('settings/register', {
-                moduleId: 'global',
-                settings: {
-                    labelInMenu: <Toggle>{
-                        type: 'toggle',
-                        default: false,
-                    },
-                    allowTelemetry: <Toggle>{
-                        type: 'toggle',
-                        default: true,
-                    },
-                    iconBg: <Color>{
-                        type: 'color',
-                        default: LSSM.$store.state.policechief
-                            ? '#004997'
-                            : '#C9302C',
-                    },
-                    iconBgAsNavBg: <Toggle>{
-                        type: 'toggle',
-                        default: false,
-                    },
-                    osmDarkTooltip: <Toggle>{
-                        type: 'toggle',
-                        default: LSSM.$store.state.darkmode,
-                        noMapkit: true,
-                        disabled: () => !LSSM.$store.state.darkmode,
-                    },
-                },
-            })
-            .then(() => {
-                new LSSM.$vue({
-                    store: LSSM.$store,
-                    i18n: LSSM.$i18n,
-                    render: h => h(LSSMMenu),
-                }).$mount(indicatorWrapper);
-
-                LSSM.$store
-                    .dispatch('settings/getSetting', {
-                        moduleId: 'global',
-                        settingId: 'osmDarkTooltip',
-                        default: true,
-                    })
-                    .then(
-                        allowDark =>
-                            !allowDark &&
-                            document.body.classList.add(
-                                'leaflet-no-dark-tooltip'
-                            )
-                    );
-            });
-    }
-
     await LSSM.$store.dispatch(
         'api/setVehicleStates',
         'core-initialVehicleStates'
@@ -141,80 +70,11 @@ require('./natives/lightbox');
         }
     }
     if (window.location.pathname === '/') {
-        telemetry(LSSM, settingId => {
-            return LSSM.$store.dispatch('settings/getSetting', {
-                moduleId: 'global',
-                settingId,
-            });
-        });
         import(
-            /* webpackChunkName: "releasenotes/main" */
-            `./modules/releasenotes/main`
-        ).then(module => module.default(LSSM));
-        // TODO: Load core modules: [support] ← Will be done in a more efficient way than polling
-
-        await LSSM.$store.dispatch('hook', {
-            event: 'radioMessage',
-            post: false,
-            callback(radioMessage: RadioMessage) {
-                if (
-                    radioMessage.type !== 'vehicle_fms' ||
-                    radioMessage.user_id !== window.user_id
-                )
-                    return;
-                const { id, fms, fms_real, user_id, caption } = radioMessage;
-                if (user_id === window.user_id) {
-                    LSSM.$store.commit('api/setVehicleState', {
-                        fms,
-                        fms_real,
-                        id,
-                        caption,
-                    });
-                }
-            },
-        });
-
-        await LSSM.$store.dispatch('hook', {
-            event: 'buildingMarkerAdd',
-            callback(buildingMarker: BuildingMarkerAdd) {
-                if (buildingMarker.user_id !== window.user_id) return;
-                const buildings = LSSM.$store.state.api.buildings as Building[];
-                const building = buildings.find(
-                    ({ id }) => id === buildingMarker.id
-                );
-                if (!building || building.caption !== buildingMarker.name) {
-                    LSSM.$store
-                        .dispatch('api/fetchBuilding', {
-                            id: buildingMarker.id,
-                            feature: 'core-buildingMarkerAdd',
-                        })
-                        .then(building =>
-                            LSSM.$store
-                                .dispatch('api/fetchVehiclesAtBuilding', {
-                                    id: building.id,
-                                    feature: 'core-buildingMarkerAdd',
-                                })
-                                .then(
-                                    async () =>
-                                        await LSSM.$store.dispatch(
-                                            'event/dispatchEvent',
-                                            await LSSM.$store.dispatch(
-                                                'event/createEvent',
-                                                {
-                                                    name: 'buildingMarkerAdd',
-                                                    detail: {
-                                                        marker: buildingMarker,
-                                                        building,
-                                                    },
-                                                }
-                                            )
-                                        )
-                                )
-                        );
-                }
-            },
-        });
+            /* webpackChunkName: "mainpageCore" */ './mainpageCore'
+        ).then(core => core.default(LSSM));
     }
+
     LSSM.$store
         .dispatch('storage/get', {
             key: 'activeModules',
---
+++ src/mainpageCore.ts
@@ -0,0 +1,143 @@
+import { Color, Toggle } from 'typings/Setting';
+import LSSMMenu from './LSSM-Menu.vue';
+import telemetry from './modules/telemetry/main';
+import { BuildingMarkerAdd, RadioMessage } from 'typings/Ingame';
+import { Building } from 'typings/Building';
+
+export default async (LSSM: Vue): Promise<void> => {
+    window.console.info(
+        `Running %cLSSM%c in Version %c${VERSION}%c`,
+        'font-weight: bold;',
+        'font-weight: normal;',
+        'font-style: italic;'
+    );
+
+    const indicatorWrapper = document.createElement('li') as HTMLLIElement;
+    document
+        .querySelector('.navbar-default .navbar-right')
+        ?.appendChild(indicatorWrapper);
+
+    LSSM.$store
+        .dispatch('settings/register', {
+            moduleId: 'global',
+            settings: {
+                labelInMenu: <Toggle>{
+                    type: 'toggle',
+                    default: false,
+                },
+                allowTelemetry: <Toggle>{
+                    type: 'toggle',
+                    default: true,
+                },
+                iconBg: <Color>{
+                    type: 'color',
+                    default: LSSM.$store.state.policechief
+                        ? '#004997'
+                        : '#C9302C',
+                },
+                iconBgAsNavBg: <Toggle>{
+                    type: 'toggle',
+                    default: false,
+                },
+                osmDarkTooltip: <Toggle>{
+                    type: 'toggle',
+                    default: LSSM.$store.state.darkmode,
+                    noMapkit: true,
+                    disabled: () => !LSSM.$store.state.darkmode,
+                },
+            },
+        })
+        .then(() => {
+            new LSSM.$vue({
+                store: LSSM.$store,
+                i18n: LSSM.$i18n,
+                render: h => h(LSSMMenu),
+            }).$mount(indicatorWrapper);
+
+            LSSM.$store
+                .dispatch('settings/getSetting', {
+                    moduleId: 'global',
+                    settingId: 'osmDarkTooltip',
+                    default: true,
+                })
+                .then(
+                    allowDark =>
+                        !allowDark &&
+                        document.body.classList.add('leaflet-no-dark-tooltip')
+                );
+        });
+
+    telemetry(LSSM, settingId => {
+        return LSSM.$store.dispatch('settings/getSetting', {
+            moduleId: 'global',
+            settingId,
+        });
+    });
+    import(
+        /* webpackChunkName: "releasenotes/main" */
+        `./modules/releasenotes/main`
+    ).then(module => module.default(LSSM));
+    // TODO: Load core modules: [support] ← Will be done in a more efficient way than polling
+
+    await LSSM.$store.dispatch('hook', {
+        event: 'radioMessage',
+        post: false,
+        callback(radioMessage: RadioMessage) {
+            if (
+                radioMessage.type !== 'vehicle_fms' ||
+                radioMessage.user_id !== window.user_id
+            )
+                return;
+            const { id, fms, fms_real, user_id, caption } = radioMessage;
+            if (user_id === window.user_id) {
+                LSSM.$store.commit('api/setVehicleState', {
+                    fms,
+                    fms_real,
+                    id,
+                    caption,
+                });
+            }
+        },
+    });
+
+    await LSSM.$store.dispatch('hook', {
+        event: 'buildingMarkerAdd',
+        callback(buildingMarker: BuildingMarkerAdd) {
+            if (buildingMarker.user_id !== window.user_id) return;
+            const buildings = LSSM.$store.state.api.buildings as Building[];
+            const building = buildings.find(
+                ({ id }) => id === buildingMarker.id
+            );
+            if (!building || building.caption !== buildingMarker.name) {
+                LSSM.$store
+                    .dispatch('api/fetchBuilding', {
+                        id: buildingMarker.id,
+                        feature: 'core-buildingMarkerAdd',
+                    })
+                    .then(building =>
+                        LSSM.$store
+                            .dispatch('api/fetchVehiclesAtBuilding', {
+                                id: building.id,
+                                feature: 'core-buildingMarkerAdd',
+                            })
+                            .then(
+                                async () =>
+                                    await LSSM.$store.dispatch(
+                                        'event/dispatchEvent',
+                                        await LSSM.$store.dispatch(
+                                            'event/createEvent',
+                                            {
+                                                name: 'buildingMarkerAdd',
+                                                detail: {
+                                                    marker: buildingMarker,
+                                                    building,
+                                                },
+                                            }
+                                        )
+                                    )
+                            )
+                    );
+            }
+        },
+    });
+};
