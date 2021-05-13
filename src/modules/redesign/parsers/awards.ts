13e6561412fdd5b213402e24f612d3b35df8c409
jan <jan.boehmerle@ledbrain.de>
2021-05-13 16:40:15 +0200

    🐛 [redesign] awards allow filtered via URL

--- src/modules/redesign/parsers/awards.ts
+++ src/modules/redesign/parsers/awards.ts
@@ -12,36 +12,41 @@ export interface AwardsWindow {
     awards: Award[];
 }
 
-export default <RedesignParser<AwardsWindow>>(({ doc, href = '' }) => ({
-    awards: Array.from(
-        doc.querySelectorAll<HTMLDivElement>('.col-sm-3 > .panel')
-    ).map(award => ({
-        caption:
-            award
-                .querySelector<HTMLDivElement>('.panel-heading .panel-title')
-                ?.textContent?.trim() ?? '',
-        image:
-            award.querySelector<HTMLImageElement>('.panel-body img')?.src ?? '',
-        desc:
-            Array.from(
-                award.querySelector<HTMLDivElement>('.panel-body')
-                    ?.childNodes ?? []
-            )
-                .find(
-                    ({ nodeType, nextSibling }) =>
-                        nodeType === 3 &&
-                        nextSibling?.nodeType === 1 &&
-                        (nextSibling as HTMLElement).matches(
-                            'div.progress, div.label-award-gold'
-                        )
+export default <RedesignParser<AwardsWindow>>(({ doc }) => ({
+    awards: Array.from(doc.querySelectorAll<HTMLDivElement>('.panel')).map(
+        award => ({
+            caption:
+                award
+                    .querySelector<HTMLDivElement>(
+                        '.panel-heading .panel-title'
+                    )
+                    ?.textContent?.trim() ?? '',
+            image:
+                award.querySelector<HTMLImageElement>('.panel-body img')?.src ??
+                '',
+            desc:
+                Array.from(
+                    award.querySelector<HTMLDivElement>('.panel-body')
+                        ?.childNodes ?? []
                 )
-                ?.textContent?.trim() ?? '',
-        finished: !!award.querySelector<HTMLDivElement>('.label-award-gold'),
-        progress:
-            award
-                .querySelector<HTMLDivElement>('.progress')
-                ?.nextSibling?.textContent?.trim()
-                .match(/\d+/g)
-                ?.map(n => parseInt(n)) ?? null,
-    })),
+                    .find(
+                        ({ nodeType, nextSibling }) =>
+                            nodeType === 3 &&
+                            nextSibling?.nodeType === 1 &&
+                            (nextSibling as HTMLElement).matches(
+                                'div.progress, div.label-award-gold'
+                            )
+                    )
+                    ?.textContent?.trim() ?? '',
+            finished: !!award.querySelector<HTMLDivElement>(
+                '.label-award-gold'
+            ),
+            progress:
+                award
+                    .querySelector<HTMLDivElement>('.progress')
+                    ?.nextSibling?.textContent?.trim()
+                    .match(/\d+/g)
+                    ?.map(n => parseInt(n)) ?? null,
+        })
+    ),
 }));
