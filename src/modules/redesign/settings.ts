f873552c5263f2b66851c321d649cb88bafe5c55
JRH-1997 <65117490+JRH-1997@users.noreply.github.com>
2021-05-28 20:24:07 +0200

    :sparkles: [redesign] Add settings for routeChecks

--- src/modules/redesign/settings.ts
+++ src/modules/redesign/settings.ts
@@ -1,5 +1,5 @@
-import { Hidden } from 'typings/Setting';
 import { ModuleSettingFunction } from 'typings/Module';
+import { Hidden, Toggle } from 'typings/Setting';
 
 export default (() => ({
     'alliances': <Hidden>{
@@ -33,5 +33,29 @@ export default (() => ({
     'verband/protokoll': <Hidden>{
         type: 'hidden',
         default: {},
+    },
+    'category.alliance': <Toggle>{
+        type: 'toggle',
+        default: true,
+    },
+    'category.credits': <Toggle>{
+        type: 'toggle',
+        default: true,
+    },
+    'category.profile': <Toggle>{
+        type: 'toggle',
+        default: true,
+    },
+    'category.vehicles': <Toggle>{
+        type: 'toggle',
+        default: true,
+    },
+    'category.einsaetze': <Hidden>{
+        type: 'hidden',
+        default: true,
+    },
+    'category.toplist': <Toggle>{
+        type: 'toggle',
+        default: true,
     },
 })) as ModuleSettingFunction;
