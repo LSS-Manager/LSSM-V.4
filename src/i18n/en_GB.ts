82ab21365070db6e06bef6f5d2824fad54a25e54
Jan <jan.boehmerle@ledbrain.de>
2021-06-02 21:54:40 +0200

    ✏️🌐 en_GB & en_AU fix copy & pasta typos

--- src/i18n/en_GB.ts
+++ src/i18n/en_GB.ts
@@ -728,7 +728,7 @@ export default {
             maxBuildings: 4,
             maxLevel: 0,
             special:
-                'You can station as many of your own vehicles as you like at a staging area, members of the association can use the room. A staging area remains for 24 hours, but you can reset it to 24 hours at any time.With Premium Account you can have 8 stating areas at the same time',
+                'You can station as many of your own vehicles as you like at a staging area, members of the association can use the room. A staging area remains for 24 hours, but you can reset it to 24 hours at any time.With Premium Account you can have 8 staging areas at the same time',
             startPersonnel: 0,
             startVehicles: [],
             maxBuildingsFunction: (): number => 4,
