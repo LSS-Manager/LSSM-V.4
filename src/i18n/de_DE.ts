1f7f9717ea26c408087950b0371c138b85e7d9c1
Tobias <Suchty112@users.noreply.github.com>
2021-08-23 12:52:34 +0200

    🚀[ep] add new poi Campingplatz

--- src/i18n/de_DE.ts
+++ src/i18n/de_DE.ts
@@ -2482,6 +2482,7 @@ export default {
         'Klärwerk',
         'Innenstadt',
         'Möbelhaus',
+        'Campingplatz',
     ],
     only_alliance_missions: [41, 43, 59, 75, 99, 207, 221, 222, 256, 350],
     transfer_missions: [147, 613],
