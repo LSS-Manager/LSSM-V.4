ba9c318aeb26f9d7d7615f48cbc2aa6736f039b0
rikivideo9 <tocchettoriccardo@gmail.com>
2021-09-14 12:55:48 +0200

    Fixed Draggable Clock Overlay

--- src/modules/clock/main.ts
+++ src/modules/clock/main.ts
@@ -1,5 +1,6 @@
 import { ModuleMainFunction } from 'typings/Module';
 import moment from 'moment';
+import { bounds } from 'leaflet';
 
 export default (async (LSSM, MODULE_ID) => {
     const getSetting = (settingId: string) =>
@@ -34,7 +35,7 @@ export default (async (LSSM, MODULE_ID) => {
         const { x, y } = await LSSM.$store.dispatch('settings/getSetting', {
             moduleId: MODULE_ID,
             settingId: 'overlayPosition',
-            defaultValue: { x: '10px', y: '10px' },
+            defaultValue: { x: '0px', y: '0px' },
         });
         clock.style.top = y;
         clock.style.left = x;
@@ -45,13 +46,14 @@ export default (async (LSSM, MODULE_ID) => {
             drag.dataTransfer.effectAllowed = 'move';
             drag.dataTransfer.setDragImage(clock, 0, 0);
         });
-        clock.addEventListener('drag', drag => {
-            const bounds = clock.getBoundingClientRect();
-            clock.style.top = `${drag.screenY - bounds.height / 2}px`;
-            clock.style.left = `${drag.screenX - bounds.width / 2}px`;
-        });
-        clock.addEventListener('dragend', async drag => {
+        /*clock.addEventListener('drag', drag => {
+            //const bounds = clock.getBoundingClientRect();
             clock.style.top = `${drag.screenY}px`;
+            clock.style.left = `${drag.screenX}px`;
+        });*/
+        clock.addEventListener('dragend', async drag => {
+            const bounds = clock.getBoundingClientRect();
+            clock.style.top = `${drag.screenY - bounds.height * 2.6}px`;
             clock.style.left = `${drag.screenX}px`;
             await LSSM.$store.dispatch('settings/setSetting', {
                 moduleId: MODULE_ID,
