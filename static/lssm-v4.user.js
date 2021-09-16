a51130fdcc375a136a642ef92f8e90dba70983a7
rikivideo9 <tocchettoriccardo@gmail.com>
2021-09-16 12:49:03 +0200

    Fix my mistake

--- static/lssm-v4.user.js
+++ static/lssm-v4.user.js
@@ -4,11 +4,11 @@
 // @author       Aisaka | Sanni | Jan (jxn_30) | Ron31
 // @description  Das Tool für das LSS in der Version 4
 // @include      /^https?:\/\/(?:w{3}\.)?(?:(policie\.)?operacni-stredisko\.cz|(politi\.)?alarmcentral-spil\.dk|(polizei\.)?leitstellenspiel\.de|(?:(police\.)?missionchief-australia|(police\.)?missionchief|(poliisi\.)?hatakeskuspeli|missionchief-japan|missionchief-korea|(politiet\.)?nodsentralspillet|(politie\.)?meldkamerspel|operador193|(policia\.)?jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|(police\.)?missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|(police\.)?operateur112\.fr|(polizia\.)?operatore112\.it|(policja\.)?operatorratunkowy\.pl|dispetcher112\.ru|(polis\.)?larmcentralen-spelet\.se)\/.*$/
-// @homepage     https://ron.v4.lssm.ledbrain.de/
-// @updateURL    https://ron.v4.lssm.ledbrain.de/lssm-v4.user.js
-// @downloadURL  https://ron.v4.lssm.ledbrain.de/lssm-v4.user.js
+// @homepage     https://proxy.lss-manager.de/v4/
+// @updateURL    https://proxy.lss-manager.de/v4/lssm-v4.user.js
+// @downloadURL  https://proxy.lss-manager.de/v4/lssm-v4.user.js
 // @run-at       document-idle
-// @icon         https://ron.v4.lssm.ledbrain.de/docs/img/lssm.png
-// @supportURL   https://ron.v4.lssm.ledbrain.de/docs/de_DE/error_report
+// @icon         https://proxy.lss-manager.de/v4/docs/img/lssm.png
+// @supportURL   https://proxy.lss-manager.de/v4/docs/de_DE/error_report
 // ==/UserScript==
-var _a,loadLSSM=function(){var e=document.createElement("script");e.src="https://ron.v4.lssm.ledbrain.de/core.js?_="+(new Date).getTime()+"&uid="+I18n.locale+"-"+user_id,e.setAttribute("type","module"),e.setAttribute("async",""),document.body.appendChild(e)};window.frameElement&&!(null===(_a=window.frameElement)||void 0===_a?void 0:_a.src.startsWith("https"))||"undefined"==typeof user_id||"undefined"==typeof I18n||(window!==window.parent&&window.parent.hasOwnProperty("lssmv4-redesign-lightbox")?(window.parent.addEventListener("lssmv4-redesign-iframe-trigger-lssm-load",loadLSSM),window.parent["lssmv4-redesign-lightbox"].src=new URL(window.location.href).toString()):loadLSSM());
+var _a,loadLSSM=function(){var e=document.createElement("script");e.src="https://proxy.lss-manager.de/v4/core.js?_="+(new Date).getTime()+"&uid="+I18n.locale+"-"+user_id,e.setAttribute("type","module"),e.setAttribute("async",""),document.body.appendChild(e)};window.frameElement&&!(null===(_a=window.frameElement)||void 0===_a?void 0:_a.src.startsWith("https"))||"undefined"==typeof user_id||"undefined"==typeof I18n||(window!==window.parent&&window.parent.hasOwnProperty("lssmv4-redesign-lightbox")?(window.parent.addEventListener("lssmv4-redesign-iframe-trigger-lssm-load",loadLSSM),window.parent["lssmv4-redesign-lightbox"].src=new URL(window.location.href).toString()):loadLSSM());
