06926eea14c1f5c8d26cc9e52d0de29e94d5dc50
rikivideo9 <tocchettoriccardo@gmail.com>
2021-09-16 12:24:00 +0200

    Fix

--- src/config.ts
+++ src/config.ts
@@ -183,5 +183,5 @@ export default {
         'core-modules': ['telemetry', 'releasenotes', 'support'],
     },
     prefix: 'lssmv4',
-    server: 'https://proxy.lss-manager.de/v4/',
+    server: 'https://ron.v4.lssm.ledbrain.de/',
 } as Config;
