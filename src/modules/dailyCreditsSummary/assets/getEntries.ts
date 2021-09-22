78a2008d570a37065359ea2170f725ac17937d45
JRH-1997 <65117490+JRH-1997@users.noreply.github.com>
2021-09-23 00:21:18 +0200

    [dcs] Count alternate_version as mission

--- src/modules/dailyCreditsSummary/assets/getEntries.ts
+++ src/modules/dailyCreditsSummary/assets/getEntries.ts
@@ -16,7 +16,7 @@ export default async (
     })) as Mission[];
 
     const missionsString = missions
-        .map(({ name }) => LSSM.$utils.escapeRegex(name))
+        .flatMap(({ name, alternate_version }) => alternate_version ? [LSSM.$utils.escapeRegex(name), LSSM.$utils.escapeRegex(alternate_version.mission_type.name)] : LSSM.$utils.escapeRegex(name))
         .join('|');
 
     const credits_types: CreditsTypes = (
