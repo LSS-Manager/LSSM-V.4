61842dc7273371df519d5c5dff45435bec55c4d2
Tobias <Suchty112@users.noreply.github.com>
2021-10-02 15:37:13 +0200

    🚀[ep] add new poi Kompostieranlage

--- src/i18n/de_DE.ts
+++ src/i18n/de_DE.ts
@@ -2491,6 +2491,7 @@ export default {
         'Innenstadt',
         'Möbelhaus',
         'Campingplatz',
+        'Kompostieranlage',
     ],
     only_alliance_missions: [41, 43, 59, 75, 99, 207, 221, 222, 256, 350],
     transfer_missions: [147, 613],
