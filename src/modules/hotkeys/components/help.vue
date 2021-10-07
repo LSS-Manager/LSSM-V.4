b082ba1b35184c84437787badeec69b299158fd5
Jan <jan.boehmerle@ledbrain.de>
2021-10-07 18:37:31 +0200

    ✨ [hotkeys] add sorting and searching to overview

--- src/modules/hotkeys/components/help.vue
+++ src/modules/hotkeys/components/help.vue
@@ -5,31 +5,20 @@
             :head="{
                 command: {
                     title: $t('modules.hotkeys.settings.command'),
-                    noSort: true,
                 },
                 hotkey: {
                     title: $t('modules.hotkeys.settings.hotkey'),
-                    noSort: true,
                 },
             }"
-            no-search
             :table-attrs="{ class: 'table table-striped' }"
+            :search="search"
+            @search="s => (search = s)"
+            :sort="sort"
+            :sort-dir="sortDir"
+            @sort="setSort"
         >
-            <tr>
-                <th>{{ getCommandName('*.help') }}</th>
-                <td class="hotkey-wrapper">
-                    <input
-                        type="text"
-                        readonly="readonly"
-                        class="form-control"
-                        :style="`--length: 2ch;`"
-                        disabled
-                        value="f1"
-                    />
-                </td>
-            </tr>
-            <tr v-for="hotkey in hotkeys" :key="hotkey.command">
-                <th>{{ getCommandName(hotkey.command) }}</th>
+            <tr v-for="hotkey in hotkeysSorted" :key="hotkey.command">
+                <th>{{ hotkey.command }}</th>
                 <td class="hotkey-wrapper">
                     <input
                         type="text"
@@ -48,21 +37,21 @@
 <script lang="ts">
 import Vue from 'vue';
 
-import {
-    DefaultComputed,
-    DefaultData,
-    DefaultMethods,
-} from 'vue/types/options';
+interface Hotkey {
+    command: string;
+    hotkey: string;
+}
 
 export default Vue.extend<
-    DefaultData<Vue>,
-    DefaultMethods<Vue>,
-    DefaultComputed,
+    { search: string; sort: keyof Hotkey; sortDir: 'asc' | 'desc' },
+    { setSort(key: keyof Hotkey): void },
     {
-        hotkeys: {
-            command: string;
-            hotkey: string;
-        }[];
+        namedHotkeys: Hotkey[];
+        hotkeysFiltered: Hotkey[];
+        hotkeysSorted: Hotkey[];
+    },
+    {
+        hotkeys: Hotkey[];
         getCommandName(command: string): string;
     }
 >({
@@ -76,6 +65,53 @@ export default Vue.extend<
             import(
                 /* webpackChunkName: "components/enhanced-table" */ '../../../components/enhanced-table.vue'
             ),
+    },
+    data() {
+        return {
+            search: '',
+            sort: 'command',
+            sortDir: 'asc',
+        };
+    },
+    computed: {
+        namedHotkeys() {
+            return [
+                { command: this.getCommandName('*.help'), hotkey: 'f1' },
+                ...this.hotkeys.map(hotkey => ({
+                    ...hotkey,
+                    command: this.getCommandName(hotkey.command),
+                })),
+            ];
+        },
+        hotkeysFiltered() {
+            const search = this.search.trim().toLowerCase();
+            return search
+                ? this.namedHotkeys.filter(({ command, hotkey }) =>
+                      `${command}:${hotkey}`.toLowerCase().match(search)
+                  )
+                : this.namedHotkeys;
+        },
+        hotkeysSorted() {
+            return (this.search.trim().toLowerCase()
+                ? this.hotkeysFiltered
+                : this.namedHotkeys
+            ).sort((a, b) => {
+                let modifier = 1;
+                if (this.sortDir === 'desc') modifier = -1;
+                const left = a[this.sort];
+                const right = b[this.sort];
+                if (left < right) return -1 * modifier;
+                if (left > right) return modifier;
+                return 0;
+            });
+        },
+    },
+    methods: {
+        setSort(s) {
+            this.sortDir =
+                s === this.sort && this.sortDir === 'asc' ? 'desc' : 'asc';
+            this.sort = s;
+        },
     },
     props: {
         hotkeys: {
