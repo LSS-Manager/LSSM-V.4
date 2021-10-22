1290aa2f68a2fa4b0ba3c419a48ba3ad7a687227
Jan <jan.boehmerle@ledbrain.de>
2021-10-22 10:32:49 +0200

    🚑💚 force node 16 on master too

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node # install latest node version
+nvm install 16 # install node 16
+nvm use 16
 
 # version output helps when there are debugging needs
 echo "node: $(node -v) – npm: $(npm -v) – yarn: $(yarn -v) – nvm: $(nvm -v)"
