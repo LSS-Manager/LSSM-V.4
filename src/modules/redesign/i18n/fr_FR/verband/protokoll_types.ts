bc1d8e844739b5b0160dfe1473ff53ebc06a1186
Jan <jan.boehmerle@ledbrain.de>
2021-11-19 22:28:58 +0100

    🐛🌐 [redesign/verband] fix translation error

--- src/modules/redesign/i18n/fr_FR/verband/protokoll_types.ts
+++ src/modules/redesign/i18n/fr_FR/verband/protokoll_types.ts
@@ -1,12 +1,12 @@
 export default {
     mission: { regex: /Mission majeur lancée/ },
-    event: { regex: /Évènement d\'alliance lancé/ },
-    appl_accepted: { regex: /Ajouté à l\'alliance/ },
-    appl_declined: { regex: /Refusé à l\'alliance/ },//à vérifié
-    deny_appl: { regex: /Interdit de postuler à l\'alliance/ },
-    allow_appl: { regex: /Autorisé à postuler à l\'alliance/ },
-    left: { regex: /A quitté l\'alliance/ },
-    kicked: { regex: /A été explusé de l\'alliance/ },
+    event: { regex: /Évènement d'alliance lancé/ },
+    appl_accepted: { regex: /Ajouté à l'alliance/ },
+    appl_declined: { regex: /Refusé à l'alliance/ },//à vérifié
+    deny_appl: { regex: /Interdit de postuler à l'alliance/ },
+    allow_appl: { regex: /Autorisé à postuler à l'alliance/ },
+    left: { regex: /A quitté l'alliance/ },
+    kicked: { regex: /A été explusé de l'alliance/ },
     set_chatban: { regex: /Bannisement de salon instauré/ },
     remove_chatban: { regex: /Bannisement de salon retiré/ },
     added_role: { regex: /Nommé .*?/, title: 'Role AJouté' },
