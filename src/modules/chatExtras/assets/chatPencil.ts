a1834bdd5506a107f9cdc406d666f65201f32cbb
Notme112 <84899746+Notme112@users.noreply.github.com>
2021-11-28 16:54:39 +0100

    Delete chatPencil.ts

--- src/modules/chatExtras/assets/chatPencil.ts
+++
@@ -1,9 +0,0 @@
-export default (LSSM: Vue): void => {
-
-    document
-        .querySelectorAll<HTMLSpanElement>('.mission_chat_message_username')
-        .forEach(chatMessageLabel => {
-            chatMessageLabel.setAttribute(
-                'innerHTML', '<span class="fa fa-pencil-alt"></span>'
-            );
-};
