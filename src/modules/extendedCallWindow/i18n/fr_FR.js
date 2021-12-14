6b5d680a8268cc130a4a4558d08fc29a9a639332
deoxis9001 <deoxis9001@pokemon-triforce.ze.cx>
2021-12-14 09:12:03 +0100

    Update regex FR

--- src/modules/extendedCallWindow/i18n/fr_FR.js
+++ src/modules/extendedCallWindow/i18n/fr_FR.js
@@ -89,7 +89,7 @@ module.exports = {
         water: 'l d’eau',
         staffPrefix: ['Nous avons besoin de'],
         staff: {
-            [/^pompiers en plus$/]: [0, 1, 12, 13, 15, 32, 33, 34],
+            [/^pompier(s)? en plus$/]: [0, 1, 12, 13, 15, 32, 33, 34],
         },
         vehiclesByRequirement: {
             [/^fourgon(s)? d’incendie$/]: [0, 1, 12, 13, 15, 32, 33, 34],
