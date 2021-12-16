89cf973b30326e3b3c375e5d5e83906e4685494e
Tsumiki <mirkoiz@hotmail.de>
2021-12-16 11:50:04 +0100

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
 # Deploy data using rsync
-rsync -e "ssh -o LogLevel=error -o StrictHostKeyChecking=no -p $SERVER_PORT" -r --delete-after "$WORK_DIR"/dist/ "$SERVER_USER"@"$SERVER":"$DEPLOY_DIR"
+rsync -r --delete-after "$WORK_DIR"/dist/ "$DEPLOY_DIR"
 # Copy static configuration
-ssh -o LogLevel=error -o StrictHostKeyChecking=no -p "$SERVER_PORT" "$SERVER_USER"@"$SERVER" "cp -r $DIR_STATIC $DEPLOY_DIR"
+/bin/cp -rf $DIR_STATIC $DEPLOY_DIR
