78dff55ab3fecc9b3f70b9fa567dee9ada0dfcb7
Jan <jan.boehmerle@ledbrain.de>
2021-12-30 15:01:18 +0100

    💚 [deploy] fix deploying static folder

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,7 @@
 # Deploy data using rsync
-rsync -r --delete-after "$WORK_DIR"/dist/ "$DEPLOY_DIR"
+rsync -r --delete-after --exlude "$WORK_DIR"/dist/static/* "$WORK_DIR"/dist/ "$DEPLOY_DIR"
 # Copy static configuration
-cp -rf "$DIR_STATIC"/* $DEPLOY_DIR
+cp -rf "$DIR_STATIC"/* "$DEPLOY_DIR"
+rsync -r "$WORK_DIR"/dist/static/ "$DEPLOY_DIR"/static
 
 exit 0
