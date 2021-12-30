087b7532860fad8738c80b12e7bfa7a8e6fbeb00
Jan <jan.boehmerle@ledbrain.de>
2021-12-30 15:11:40 +0100

    💚 [deploy] fix deploying

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,5 +1,5 @@
 # Deploy data using rsync
-rsync -r --delete-after --exlude "$WORK_DIR"/dist/static/* "$WORK_DIR"/dist/ "$DEPLOY_DIR"
+rsync -r --delete-after --exclude "$WORK_DIR"/dist/static/* "$WORK_DIR"/dist/ "$DEPLOY_DIR"
 # Copy static configuration
 cp -rf "$DIR_STATIC"/* "$DEPLOY_DIR"
 rsync -r "$WORK_DIR"/dist/static/ "$DEPLOY_DIR"/static
