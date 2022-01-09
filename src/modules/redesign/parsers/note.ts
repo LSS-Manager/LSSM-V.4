83cffc0715d7b3029999be2ba02db4abe840c75c
Jan <jan.boehmerle@ledbrain.de>
2022-01-09 15:22:58 +0100

    🚨 [redesign/note] remove linter warning

--- src/modules/redesign/parsers/note.ts
+++ src/modules/redesign/parsers/note.ts
@@ -4,11 +4,6 @@ export interface NoteWindow {
     note: string;
 }
 
-export default <RedesignParser<NoteWindow>>(({
-    doc,
-    getIdFromEl = () => -1,
-}) => {
-    return {
-        note: doc.querySelector('#note_message')?.textContent,
-    };
-});
+export default <RedesignParser<NoteWindow>>(({ doc }) => ({
+    note: doc.querySelector('#note_message')?.textContent,
+}));
