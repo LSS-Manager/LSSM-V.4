da1d737440ea80cb1d4bd99550fa735e69e77c60
Jan <jan.boehmerle@ledbrain.de>
2022-01-18 00:34:23 +0100

    🐛 [sap] fix NL Zipcodes

--- src/modules/shareAlliancePost/main.ts
+++ src/modules/shareAlliancePost/main.ts
@@ -84,7 +84,7 @@ export default <ModuleMainFunction>(async ({ LSSM, MODULE_ID, getSetting }) => {
     const city = addressSplit[addressSplit.length - 1]?.trim() ?? '–';
     const cityWithoutZip = city
         // matches digit-only, NL-Style and UK-Style ZIP-Codes
-        .replace(/^(\d+|(\d{4} ?[A-Z]{2})|([A-Z0-9]{2,4} [A-Z0-9]{3}))/, '')
+        .replace(/^((\d{4} ?[A-Z]{2})|\d+|([A-Z0-9]{2,4} [A-Z0-9]{3}))/, '')
         .trim();
 
     let beginAtDate = '–';
