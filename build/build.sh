ba14d6ae28164d476e82db5915ed3786e2455fde
Jan <jan.boehmerle@ledbrain.de>
2022-03-01 15:38:40 +0100

    ⬆️ use latest nvm

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y
 sudo apt-get install -y rsync zip
 # this loads nvm and enables the node version installed by nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
