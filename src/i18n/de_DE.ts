4fce7dd5b035ae16acfe25cdbdf4c80774242cbf
Tobias <Suchty112@users.noreply.github.com>
2022-03-04 10:08:22 +0100

    🌐 [de_DE] rename POI Moorland to Moor

--- src/i18n/de_DE.ts
+++ src/i18n/de_DE.ts
@@ -2952,7 +2952,7 @@ Sollten mehrere Anfragen in kurzer Zeit fehlschlagen, könnte das an Serverprobl
         'Campingplatz',
         'Kompostieranlage',
         'Textilverarbeitung',
-        'Moorland',
+        'Moor',
     ],
     only_alliance_missions: [41, 43, 59, 75, 99, 207, 221, 222, 256, 350],
     transfer_missions: [147, 613],
