65ddbed765beb8824b9606462838bd6d69990d61
Jan <jan.boehmerle@ledbrain.de>
2022-03-22 13:31:35 +0100

    🚨 [redesign] fix linter warnings

--- src/modules/redesign/parsers/messages/new.ts
+++ src/modules/redesign/parsers/messages/new.ts
@@ -3,4 +3,4 @@ import type { RedesignParser } from 'typings/modules/Redesign';
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface NewMessageWindow {}
 
-export default <RedesignParser<NewMessageWindow>>(({ doc }) => ({}));
+export default <RedesignParser<NewMessageWindow>>(() => ({}));
