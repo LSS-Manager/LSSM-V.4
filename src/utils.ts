8077406738064790d22bd0719baaa881d91bc91a
Jan <jan.boehmerle@ledbrain.de>
2022-03-22 12:39:25 +0100

    🐛 [utils] fix chrome issue in escapeRegex

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ export default (Vue: VueConstructor): void => {
         urlRegex:
             /(?:(?:ftp|https?):)?\/\/(?:\S+@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[01])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4])|(?:(?:[\da-z\u00a1-\uffff][\w\-\u00a1-\uffff]{0,62})?[\da-z\u00a1-\uffff]\.)+[a-z\u00a1-\uffff]{2,}\.?)(?::\d{2,5})?(?:[#/?]\S*)?/giu,
         escapeRegex(s: string) {
-            return s.replace(/[$()*+.?[\\^|]/gu, '\\$&');
+            return s.replace(/[$()*+.?[\\\]^|]/gu, '\\$&');
         },
         getMissionTypeInMissionWindow() {
             const missionHelpBtn =
