52a7f8320226722d11d1a9b6d94690d8a9b0cc3e
jan <jan.boehmerle@ledbrain.de>
2022-04-09 12:31:52 +0200

    ♻️ [userscript] improve userscript

--- prebuild/buildUserscript.ts
+++ prebuild/buildUserscript.ts
@@ -15,7 +15,7 @@ export default async (): Promise<void> =>
         './static/lssm-v4.user.js',
         `// ==UserScript==
 // @name         ${script.name}
-// @version      ${packageJson.version.replace(/\+.*$/u, '')}+${Object.keys(
+// @version      ${packageJson.version.replace(/\+.*$/u, '')}-${Object.keys(
             config.games
         )
             .map(lang => lang.replace(/^.*?_/u, ''))
@@ -32,13 +32,14 @@ ${Object.values(config.games)
             }`
     )
     .join('\n')}
-// @homepage     ${config.server}
+// @homepage     ${config.server}/docs/
 // @updateURL    ${config.server}lssm-v4.user.js
 // @downloadURL  ${config.server}lssm-v4.user.js
-// @run-at       document-idle
 // @icon         ${config.server}docs/img/lssm.png
-// @supportURL   ${config.server}docs/de_DE/error_report
+// @supportURL   ${config.server}docs/en_US/error_report
+// @run-at       document-idle
 // ==/UserScript==
+/* global I18n, user_id */
 ${
     (
         await Terser.minify(fs.readFileSync('./src/userscript.js').toString(), {
