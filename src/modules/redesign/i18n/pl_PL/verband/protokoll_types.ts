a8a8bb9b47aa9558f2de2fbd020e8f0fe12d5d6a
jan <jan.boehmerle@ledbrain.de>
2022-04-25 17:45:14 +0200

    🐛 fix pl_PL json

--- src/modules/redesign/i18n/pl_PL/verband/protokoll_types.ts
+++ src/modules/redesign/i18n/pl_PL/verband/protokoll_types.ts
@@ -10,7 +10,10 @@ export default {
     set_chatban: { regex: /Ustawiono blokadę czatu/u },
     remove_chatban: { regex: /Usunięto blokadę czatu/u },
     added_role: { regex: /Ustaw jako .*?/u, title: 'Dodano role' },
-    removed_role: { regex: /Usunięto ze stanowiska .*?/u, title: 'Usunięto role' },
+    removed_role: {
+        regex: /Usunięto ze stanowiska .*?/u,
+        title: 'Usunięto role',
+    },
     start_schooling: { regex: /Utworzono kurs/u },
     complete_schooling: { regex: /Ukończono kurs/u },
     start_extension: { regex: /Rozpoczęto rozbudowę/u },
