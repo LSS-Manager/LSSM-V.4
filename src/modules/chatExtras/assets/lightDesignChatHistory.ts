91b793076bcca9c4857d55841e6c24a2c702a7bd
Notme112 <84899746+Notme112@users.noreply.github.com>
2022-05-09 21:23:38 +0200

    [ce] fixed event listener

--- src/modules/chatExtras/assets/lightDesignChatHistory.ts
+++ src/modules/chatExtras/assets/lightDesignChatHistory.ts
@@ -1,27 +1,15 @@
 export default () => {
-  const addLightChatDesign = () => {
-    let lssmActive = getSetting('lightDesignChatHistory').then((active) => {
-      document.querySelectorAll<Array>(`${lssmActive ? '#modals-container ' : ''} .well`).forEach((el) => {
-        el.classList.remove('well');
-      });
-      document.querySelectorAll<Array>(`${lssmActive ? '#modals-container ' : ''} .pull-right`).forEach((el) => {
-        el.classList.remove('pull-right');
-        el.textContent = '[' + el.textContent + ']';
-      });
-      document.querySelectorAll<Array>(`${lssmActive ? '#modals-container ' : ''} p`).forEach((el) => {
-        el.parentElement.querySelector<HTMLSpanElement>('span').append(': ' + el.textContent);
-        el.remove();
-      });
+  const addLightChatDesign = (lssmActive) => {
+    document.querySelectorAll(`${lssmActive ? '#modals-container ' : ''} .well`).forEach((el) => {
+      el.classList.remove('well');
+    });
+    document.querySelectorAll(`${lssmActive ? '#modals-container ' : ''} .pull-right`).forEach((el) => {
+      el.classList.remove('pull-right');
+      el.textContent = '[' + el.textContent + ']';
+    });
+    document.querySelectorAll(`${lssmActive ? '#modals-container ' : ''} p`).forEach((el) => {
+      el.parentElement.querySelector('span').append(': ' + el.textContent);
+      el.remove();
     });
   };
-  if(location.pathname == '/') {
-    document.addEventListener('lssmv4-event-redesign-finished-loading', (e) => {
-      if(e.detail.type == 'chat') addDesign(true);
-    });
-  } else {
-    addDesign(false);
-    document.addEventListener('lssmv4-event-redesign-finished-loading', (e) => {
-      if(e.detail.type == 'chat') addDesign(false);
-    });
-  }
-})
+}
