1684d96ce0185cb0f5173764bba74f0c3ee7bdb1
Notme112 <84899746+Notme112@users.noreply.github.com>
2022-05-09 21:32:49 +0200

    Update lightDesignChatHistory.ts

--- src/modules/chatExtras/assets/lightDesignChatHistory.ts
+++ src/modules/chatExtras/assets/lightDesignChatHistory.ts
@@ -1,13 +1,13 @@
 export default () => {
-  const addLightChatDesign = (lssmActive) => {
-    document.querySelectorAll(`${lssmActive ? '#modals-container ' : ''} .well`).forEach((el) => {
+  const addLightChatDesign = (redesignActive) => {
+    document.querySelectorAll(`${redesignActive ? '#modals-container ' : ''} .well`).forEach((el) => {
       el.classList.remove('well');
     });
-    document.querySelectorAll(`${lssmActive ? '#modals-container ' : ''} .pull-right`).forEach((el) => {
+    document.querySelectorAll(`${redesignActive ? '#modals-container ' : ''} .pull-right`).forEach((el) => {
       el.classList.remove('pull-right');
       el.textContent = '[' + el.textContent + ']';
     });
-    document.querySelectorAll(`${lssmActive ? '#modals-container ' : ''} p`).forEach((el) => {
+    document.querySelectorAll(`${redesignActive ? '#modals-container ' : ''} p`).forEach((el) => {
       el.parentElement.querySelector('span').append(': ' + el.textContent);
       el.remove();
     });
