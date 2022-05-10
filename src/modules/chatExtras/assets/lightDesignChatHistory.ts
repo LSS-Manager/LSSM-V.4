d5e017a87376a55a5ab5a6635d8e81bf324b696d
Notme112 <84899746+Notme112@users.noreply.github.com>
2022-05-10 16:13:57 +0200

    Update lightDesignChatHistory.ts

--- src/modules/chatExtras/assets/lightDesignChatHistory.ts
+++ src/modules/chatExtras/assets/lightDesignChatHistory.ts
@@ -1,16 +1,18 @@
 export default (redesignActive: boolean, modalName: string) => {
-    let startPhrase = `#modals-container [data-modal="${modalName}"] + .vm--modal .redesign-wrapper `
-    document.querySelectorAll(`${redesignActive ? startPhrase : ''}.well`).forEach((el) => {
-        el.classList.remove('well');
-    });
-    document.querySelectorAll(`${redesignActive ? startPhrase : ''}.pull-right`).forEach((el) => {
+    let startPhrase = redesignActive ? `#modals-container [data-modal="${modalName}"] + .vm--modal .redesign-wrapper ` : '';
+    document.querySelectorAll<HTMLSpanElement>(`${startPhrase}div.well span.pull-right`).forEach((el: HTMLSpanElement) => {
         el.classList.remove('pull-right');
         el.textContent = '[' + el.textContent + ']';
     });
-    document.querySelectorAll(`${redesignActive ? startPhrase : ''}p`).forEach((el) => {
+    document.querySelectorAll<HTMLDivElement>(`${startPhrase}div.well`).forEach((el: HTMLDivElement) => {
+        el.classList.remove('well');
+        el.classList.add('chat-element-container');
+    });
+    document.querySelectorAll<HTMLParagraphElement>(`${startPhrase}p.chat-element-container`).forEach((el: HTMLParagraphElement) => {
         if(!el.classList.contains('label')) {
             el.parentElement.querySelector('span').append(': ' + el.textContent);
+            el.classList.remove('chat-element-container');
             el.remove();
         };
     });
-};
+}
