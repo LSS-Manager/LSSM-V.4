b333185948b86fdc3b02222186f921f489ef5ac4
jan <jan.boehmerle@ledbrain.de>
2022-05-23 11:35:00 +0200

    ✨ [eb] schoolsBuildingFilter vehicleType

--- src/modules/extendedBuilding/assets/schoolsBuildingFilter.ts
+++ src/modules/extendedBuilding/assets/schoolsBuildingFilter.ts
@@ -1,12 +1,12 @@
-import type { Vehicle } from 'typings/Vehicle';
 import type { Building, InternalBuilding } from 'typings/Building';
+import type { InternalVehicle, Vehicle } from 'typings/Vehicle';
 
 interface BuildingInfos {
     el: HTMLDivElement;
     id: number;
     name: string;
     buildingType: number;
-    vehicleTypes: Set<number>;
+    vehicleTypes: number[];
 }
 
 export default async (LSSM: Vue) => {
@@ -33,6 +33,8 @@ export default async (LSSM: Vue) => {
 
     const buildingTypes: Record<number, InternalBuilding> =
         LSSM.$store.getters.$tBuildings;
+    const vehicleTypes: Record<number, InternalVehicle> =
+        LSSM.$store.getters.$tVehicles;
 
     accordion.querySelectorAll<HTMLDivElement>('.panel').forEach(panel => {
         const heading = panel.querySelector<HTMLDivElement>('.panel-heading');
@@ -51,9 +53,11 @@ export default async (LSSM: Vue) => {
                     ?.trim() ?? '',
             id: buildingId,
             buildingType: buildingsById[buildingId].building_type,
-            vehicleTypes: new Set(
-                vehiclesByBuilding[buildingId].map(
-                    ({ vehicle_type }) => vehicle_type
+            vehicleTypes: Array.from(
+                new Set(
+                    vehiclesByBuilding[buildingId].map(
+                        ({ vehicle_type }) => vehicle_type
+                    )
                 )
             ),
         });
@@ -62,18 +66,35 @@ export default async (LSSM: Vue) => {
     const filterWrapper = document.createElement('div');
     filterWrapper.classList.add('pull-right');
 
+    const vehicleTypeFilter = document.createElement('select');
+    const vehicleAllOption = document.createElement('option');
+    vehicleAllOption.value = '*';
+    vehicleTypeFilter.append(vehicleAllOption);
+    Array.from(new Set(buildings.flatMap(({ vehicleTypes }) => vehicleTypes)))
+        .sort((a, b) =>
+            vehicleTypes[a].caption.localeCompare(vehicleTypes[b].caption)
+        )
+        .forEach(vehicleType => {
+            const option = document.createElement('option');
+            option.value = vehicleType.toString();
+            option.textContent = vehicleTypes[vehicleType].caption;
+            vehicleTypeFilter.append(option);
+        });
+
     const buildingTypeFilter = document.createElement('select');
-    const allOption = document.createElement('option');
-    allOption.value = '*';
-    buildingTypeFilter.append(allOption);
-    new Set(buildings.map(({ buildingType }) => buildingType)).forEach(
-        buildingType => {
+    const buildingAllOption = document.createElement('option');
+    buildingAllOption.value = '*';
+    buildingTypeFilter.append(buildingAllOption);
+    Array.from(new Set(buildings.map(({ buildingType }) => buildingType)))
+        .sort((a, b) =>
+            buildingTypes[a].caption.localeCompare(buildingTypes[b].caption)
+        )
+        .forEach(buildingType => {
             const option = document.createElement('option');
             option.value = buildingType.toString();
             option.textContent = buildingTypes[buildingType].caption;
             buildingTypeFilter.append(option);
-        }
-    );
+        });
 
     const searchLabel = document.createElement('label');
     const searchInput = document.createElement('input');
@@ -83,17 +104,25 @@ export default async (LSSM: Vue) => {
 
     searchLabel.append(searchInput);
 
-    filterWrapper.append(buildingTypeFilter, searchLabel);
+    filterWrapper.append(vehicleTypeFilter, buildingTypeFilter, searchLabel);
     accordion.prepend(filterWrapper, clearfix);
 
     const filter = () => {
         const search = searchInput.value.trim().toLowerCase();
+        const vehicleTypeFilterValue = parseInt(vehicleTypeFilter.value);
         const buildingTypeFilterValue = parseInt(buildingTypeFilter.value);
-        buildings.forEach(({ name, el, buildingType }) => {
+        buildings.forEach(({ name, el, buildingType, vehicleTypes }) => {
             let show = true;
             if (search.length && !name.toLowerCase().includes(search))
                 show = false;
             if (
+                show &&
+                !Number.isNaN(vehicleTypeFilterValue) &&
+                !vehicleTypes.includes(vehicleTypeFilterValue)
+            )
+                show = false;
+            if (
+                show &&
                 !Number.isNaN(buildingTypeFilterValue) &&
                 buildingType !== buildingTypeFilterValue
             )
@@ -111,6 +140,7 @@ export default async (LSSM: Vue) => {
         nameFilterTimeout = window.setTimeout(filter, 100);
     };
 
-    searchInput.addEventListener('keyup', updateFilter);
+    vehicleTypeFilter.addEventListener('change', updateFilter);
     buildingTypeFilter.addEventListener('change', updateFilter);
+    searchInput.addEventListener('keyup', updateFilter);
 };
