2f7ae81be69e8f6b906d732d853ac6dbb963147f
jan <jan.boehmerle@ledbrain.de>
2022-05-30 13:08:48 +0200

    ✨ [ecl] fixedEventInfo

---
+++ src/modules/extendedCallList/assets/fixedEventInfo.ts
@@ -0,0 +1,34 @@
+export default (LSSM: Vue) => {
+    const eventInfo = document.querySelector<HTMLDivElement>('#eventInfo');
+    if (!eventInfo) return;
+
+    const whitespace = document.createElement('div');
+    whitespace.id = LSSM.$store.getters.nodeAttribute(
+        'ecl-fixed_event_info-whitespace'
+    );
+    whitespace.style.setProperty('height', getComputedStyle(eventInfo).height);
+    eventInfo.after(whitespace);
+
+    LSSM.$store
+        .dispatch('addStyle', {
+            selectorText: `#eventInfo`,
+            style: {
+                'position': 'absolute',
+                'width': 'calc(100% - 2 * (15px + 1px) - 18px)',
+                'z-index': 1,
+            },
+        })
+        .then();
+
+    LSSM.$store
+        .dispatch('hook', {
+            event: 'eventAnnounce',
+            callback() {
+                whitespace.style.setProperty(
+                    'height',
+                    getComputedStyle(eventInfo).height
+                );
+            },
+        })
+        .then();
+};
