deb668af04dde805d3febbd0e147be095a2123d3
Build Server <developer@lss-manager.de>
2022-06-12 18:04:26 +0100

    📦 Version 4.5.12+20220612.1901 [tc-push]

--- src/modules/chatExtras/main.ts
+++ src/modules/chatExtras/main.ts
@@ -1,20 +1,22 @@
 import type { ModuleMainFunction } from 'typings/Module';
 
 export default (async ({ LSSM, getSetting }) => {
-    if(location.pathname === '/') {
+    if (location.pathname === '/') {
         getSetting('chatTime').then(chatTime => {
             if (chatTime) {
                 import(
                     /* webpackChunkName: "modules/chatExtras/timeFormatter" */ './assets/timeFormatter'
                 ).then(async ({ default: timeFormatter }) =>
-                    timeFormatter(LSSM, await getSetting<string>('chatTimeFormat'))
+                    timeFormatter(
+                        LSSM,
+                        await getSetting<string>('chatTimeFormat')
+                    )
                 );
             }
         });
     }
 
-    if(location.pathname === '/') 
-    {
+    if (location.pathname === '/') {
         getSetting('cloneHistoryBtnToHeader').then(clone => {
             if (clone) {
                 import(
@@ -24,22 +26,27 @@ export default (async ({ LSSM, getSetting }) => {
         });
     }
 
-    getSetting('lightDesignChatHistory').then((lightChatDesignActive)  => {
-      if(lightChatDesignActive){
-        import(
-            /* webpackChunkName: "modules/chatExtras/lightDesignChatHistory" */ './assets/lightDesignChatHistory'
-        ).then(({default: addLightChatDesign}) => {
-          if(location.pathname.includes('alliance_chats')) {
-            addLightChatDesign(false, '');
-          }
-          LSSM.$store.dispatch('event/addListener', {
-            name: 'redesign-finished-loading',
-            listener(e: CustomEvent) {
-              if(e.detail.type === 'chat') addLightChatDesign(location.pathname === '/', e.detail.modalName);
-            }
-          });
-        })
-      }
+    getSetting('lightDesignChatHistory').then(lightChatDesignActive => {
+        if (lightChatDesignActive) {
+            import(
+                /* webpackChunkName: "modules/chatExtras/lightDesignChatHistory" */ './assets/lightDesignChatHistory'
+            ).then(({ default: addLightChatDesign }) => {
+                if (location.pathname.includes('alliance_chats'))
+                    addLightChatDesign(false, '');
+
+                LSSM.$store.dispatch('event/addListener', {
+                    name: 'redesign-finished-loading',
+                    listener(e: CustomEvent) {
+                        if (e.detail.type === 'chat') {
+                            addLightChatDesign(
+                                location.pathname === '/',
+                                e.detail.modalName
+                            );
+                        }
+                    },
+                });
+            });
+        }
     });
 
     getSetting('selfHighlight').then(selfHighlight => {
