7ac6c58d0eaa65bf90fe3d1f059bab27e6750f77
jan <jan.boehmerle@ledbrain.de>
2022-08-13 15:16:54 +0200

    💡 add `@file` docs to SCEditor `.d.ts` files

--- typings/SCEditor/Escape.d.ts
+++ typings/SCEditor/Escape.d.ts
@@ -1,3 +1,7 @@
+/**
+ * @file - Types for SCEditor {@link https://www.sceditor.com/}, the WYSIWYG Editor used in game.
+ */
+
 export interface Escape {
     regex(str: string): string;
     entities(str: string, noQuotes?: boolean): string;
