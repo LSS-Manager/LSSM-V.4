2207c39f0271bdf6ef6cee9c4ca44e436e61620b
jan <jan.boehmerle@ledbrain.de>
2022-11-05 15:48:55 +0100

    🏷️ [notifications] improve clickHandler types

--- typings/store/notifications/Actions.d.ts
+++ typings/store/notifications/Actions.d.ts
@@ -2,9 +2,35 @@
  * @file - Types for the actions of notification store.
  */
 
+import type { NotificationOptions } from 'vue-notification';
 import type { NotificationsState, NotificationType } from './State';
 
-export interface NotificationsSend {
+interface ClickHandlerProp<
+    Data extends Record<string, unknown> = Record<string, unknown>
+> {
+    item: Required<NotificationOptions> & {
+        data: Data & {
+            icon: string;
+            clickHandler?: ClickHandler;
+        };
+    };
+    close(): void;
+}
+
+export type ClickHandler<
+    AcceptsNullProp extends boolean = false,
+    Data extends Record<string, unknown> = Record<string, unknown>
+> = (
+    props: AcceptsNullProp extends true
+        ? ClickHandlerProp<Data> | null
+        : ClickHandlerProp<Data>,
+    $event: MouseEvent
+) => unknown;
+
+export interface NotificationsSend<
+    Data extends Record<string, unknown> = Record<string, unknown>,
+    Desktop extends boolean = boolean
+> {
     group?: NotificationsState['groups'][0];
     type?: NotificationType;
     title: string;
@@ -12,9 +38,9 @@ export interface NotificationsSend {
     icon?: string;
     duration?: number;
     speed?: number;
-    data?: Record<string, unknown>;
+    data?: Data;
     clean?: boolean;
     ingame?: boolean;
-    desktop?: boolean;
-    clickHandler?(props: unknown, event?: MouseEvent): unknown;
+    desktop?: Desktop;
+    clickHandler?: ClickHandler<Desktop extends true ? true : false, Data>;
 }
