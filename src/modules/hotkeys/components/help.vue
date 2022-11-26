a765b43f56ad849f0a3d0810ee4bdef9b317be58
jan <jan.boehmerle@ledbrain.de>
2022-11-26 09:59:42 +0100

    ♻️ [hotkeys] improve F1 view

--- src/modules/hotkeys/components/help.vue
+++ src/modules/hotkeys/components/help.vue
@@ -3,8 +3,9 @@
         <h2>{{ $t('modules.hotkeys.settings.hotkeys.title') }}</h2>
         <enhanced-table
             :head="{
-                command: {
+                name: {
                     title: $t('modules.hotkeys.settings.command'),
+                    attrs: { colspan },
                 },
                 hotkey: {
                     title: $t('modules.hotkeys.settings.hotkey'),
@@ -17,8 +18,17 @@
             :sort-dir="sortDir"
             @sort="setSort"
         >
-            <tr v-for="hotkey in hotkeysSorted" :key="hotkey.command">
-                <th>{{ hotkey.command }}</th>
+            <tr v-for="hotkey in hotkeysList" :key="hotkey.command">
+                <template v-for="(caption, index) in hotkey.name">
+                    <td
+                        :key="`${hotkey.command}-${index}`"
+                        :colspan="index < hotkey.colspan ? 1 : colspan - index"
+                        :rowspan="hotkey.rowspan[index]"
+                        v-if="hotkey.rowspan[index]"
+                    >
+                        {{ caption }}
+                    </td>
+                </template>
                 <td class="hotkey-wrapper">
                     <input
                         type="text"
@@ -40,19 +50,28 @@ import Vue from 'vue';
 interface Hotkey {
     command: string;
     hotkey: string;
+    name: string[];
+    colspan: number;
 }
 
+type Sort = 'hotkey' | 'name';
+
 export default Vue.extend<
-    { search: string; sort: keyof Hotkey; sortDir: 'asc' | 'desc' },
-    { setSort(key: keyof Hotkey): void },
+    { search: string; sort: Sort; sortDir: 'asc' | 'desc' },
+    {
+        setSort(key: Sort): void;
+        getRowSpan(command: string, index: number): number;
+    },
     {
         namedHotkeys: Hotkey[];
         hotkeysFiltered: Hotkey[];
         hotkeysSorted: Hotkey[];
+        hotkeysList: (Hotkey & { rowspan: number[] })[];
+        colspan: number;
     },
     {
-        hotkeys: Hotkey[];
-        getCommandName(command: string): string;
+        hotkeys: Omit<Hotkey, 'name'>[];
+        getCommandName(command: string): string[];
     }
 >({
     name: 'lssmv4-hotkeys-help',
@@ -69,25 +88,31 @@ export default Vue.extend<
     data() {
         return {
             search: '',
-            sort: 'command',
+            sort: 'name',
             sortDir: 'asc',
         };
     },
     computed: {
         namedHotkeys() {
             return [
-                { command: this.getCommandName('*.help'), hotkey: 'f1' },
+                {
+                    command: '*.help',
+                    name: this.getCommandName('*.help'),
+                    hotkey: 'f1',
+                    colspan: 1,
+                },
                 ...this.hotkeys.map(hotkey => ({
                     ...hotkey,
-                    command: this.getCommandName(hotkey.command),
+                    name: this.getCommandName(hotkey.command),
+                    colspan: hotkey.command.split('.').length - 1,
                 })),
             ];
         },
         hotkeysFiltered() {
             const search = this.search.trim().toLowerCase();
             return search
-                ? this.namedHotkeys.filter(({ command, hotkey }) =>
-                      `${command}:${hotkey}`.toLowerCase().match(search)
+                ? this.namedHotkeys.filter(({ name, hotkey }) =>
+                      `${name}:${hotkey}`.toLowerCase().match(search)
                   )
                 : this.namedHotkeys;
         },
@@ -101,10 +126,24 @@ export default Vue.extend<
                 if (this.sortDir === 'desc') modifier = -1;
                 const left = a[this.sort];
                 const right = b[this.sort];
+                // yes, the comparison works with arrays to. Thank you JS! :)
                 if (left < right) return -1 * modifier;
                 if (left > right) return modifier;
                 return 0;
             });
+        },
+        hotkeysList() {
+            return this.hotkeysSorted.map(hotkey => ({
+                ...hotkey,
+                rowspan: hotkey.command
+                    .split('.')
+                    .map((_, index) => this.getRowSpan(hotkey.command, index)),
+            }));
+        },
+        colspan() {
+            return Math.max(
+                ...this.hotkeys.map(h => h.command.split('.').length)
+            );
         },
     },
     methods: {
@@ -112,6 +151,30 @@ export default Vue.extend<
             this.sortDir =
                 s === this.sort && this.sortDir === 'asc' ? 'desc' : 'asc';
             this.sort = s;
+        },
+        getRowSpan(command, index) {
+            if (this.sort !== 'name') return 1;
+            let rowSpan = 0;
+            let passedHotkey = false;
+            for (const hotkey of this.hotkeysSorted) {
+                if (hotkey.command === command) {
+                    passedHotkey = true;
+                    rowSpan++;
+                    continue;
+                }
+                if (
+                    hotkey.command.startsWith(
+                        command
+                            .split('.')
+                            .splice(0, index + 1)
+                            .join('.')
+                    )
+                ) {
+                    if (!passedHotkey) break;
+                    else rowSpan++;
+                }
+            }
+            return rowSpan;
         },
     },
     props: {
