f298e5380109effaeaec40c1fd94306dfb4ed3b2
jan <jan.boehmerle@ledbrain.de>
2023-03-07 15:51:01 +0100

    ♻️ [redesign/parsers] fix inefficient regex

--- src/modules/redesign/parsers/profile.ts
+++ src/modules/redesign/parsers/profile.ts
@@ -1,3 +1,6 @@
+import { full } from 'acorn-walk';
+import { type Node as AcornNode, parse } from 'acorn';
+
 import type { BuildingMarkerAdd } from 'typings/Ingame';
 import type { RedesignParser } from 'typings/modules/Redesign';
 
@@ -30,6 +33,25 @@ export interface ProfileWindow {
     ban: number[];
     can_alliance_ignore: boolean;
     alliance_ignored: boolean;
+}
+
+interface CallExpressionNode extends AcornNode {
+    expression: {
+        type: 'CallExpression';
+        callee: AcornNode & { name: string };
+        arguments: (AcornNode & {
+            properties: (AcornNode & {
+                key: { value: string };
+                value:
+                    | {
+                          type: 'UnaryExpression';
+                          operator: string;
+                          argument: AcornNode & { value: number };
+                      }
+                    | { type: 'Literal'; value: number | string };
+            })[];
+        })[];
+    };
 }
 
 export default <RedesignParser<ProfileWindow>>(({ LSSM, doc, href = '' }) => {
@@ -98,15 +120,42 @@ export default <RedesignParser<ProfileWindow>>(({ LSSM, doc, href = '' }) => {
                     ?.textContent?.trim() ?? '',
         })),
         has_map: !!doc.querySelector<HTMLDivElement>('#profile_map'),
-        buildings: (
-            Array.from(doc.scripts)
-                .flatMap(script =>
-                    script.textContent?.match(
-                        /(?<=buildingMarkerAdd\()\{(?:"[^"]*":(?:\d+(?:\.\d+)?|".*?"),?)+\}(?=\);)/gu
-                    )
-                )
-                .filter(b => !!b) as string[]
-        ).map(b => JSON.parse(b)),
+        buildings: Array.from(doc.scripts)
+            .filter(script => script.textContent?.includes('buildingMarkerAdd'))
+            .flatMap(script => {
+                const tree = parse(script.textContent ?? '', {
+                    ecmaVersion: 'latest',
+                });
+
+                const markerNodes: CallExpressionNode[] = [];
+                full(
+                    tree,
+                    (node: AcornNode | CallExpressionNode) =>
+                        node.type === 'ExpressionStatement' &&
+                        'expression' in node &&
+                        node.expression.type === 'CallExpression' &&
+                        node.expression.callee.type === 'Identifier' &&
+                        node.expression.callee.name === 'buildingMarkerAdd' &&
+                        markerNodes.push(node)
+                );
+
+                return markerNodes.map(
+                    node =>
+                        Object.fromEntries(
+                            node.expression.arguments[0].properties.map(
+                                prop => [
+                                    prop.key.value,
+                                    prop.value.type === 'UnaryExpression'
+                                        ? parseInt(
+                                              prop.value.operator +
+                                                  prop.value.argument.value
+                                          )
+                                        : prop.value.value,
+                                ]
+                            )
+                        ) as unknown as Building
+                );
+            }),
         ignored: !!doc.querySelector<HTMLAnchorElement>(
             'a[href^="/ignoriert/entfernen/"]'
         ),
