c045ebb3c5cd459b4fad8ca35ee4a6936b7d2d78
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2023-05-13 12:51:17 +0200

    ✏[ecw] implemented feedback

--- src/modules/extendedCallWindow/assets/vehicleListPermanentSearch.ts
+++ src/modules/extendedCallWindow/assets/vehicleListPermanentSearch.ts
@@ -1,22 +1,20 @@
 export default (): void => {
-    const vehicleTabs = document.querySelector('#tabs');
-    if (vehicleTabs) vehicleTabs.addEventListener('click', vehicleListChange);
-    //Initial expanding
-    vehicleListChange()
-
     /**
      * Gets called on every vehicle tab change to update the vehicle search bar
      * (for some reasons they have different search bars instead of filters).
      */
     function vehicleListChange() {
-        const vehicleSearch = document
-            .querySelector('#vehicle_list_step>.tab-content>.active')
-            ?.querySelector(
-                "[id^='mission_vehicle_search_outer_']"
-            ) as HTMLInputElement;
-        const enableSearchBtn = document
-            .querySelector('#vehicle_list_step>.tab-content>.active')
-            ?.querySelector("[id^='show_search_']") as HTMLAnchorElement;
-        if (vehicleSearch.offsetParent === null) enableSearchBtn.click();
+        const vehicleSearch = document.querySelector<HTMLInputElement>(
+            '#vehicle_list_step>.tab-content>.active [id^="mission_vehicle_search_outer_"]'
+        );
+        const enableSearchBtn = document.querySelector<HTMLAnchorElement>(
+            "#vehicle_list_step>.tab-content>.active [id^='show_search_']"
+        );
+        if (vehicleSearch?.offsetParent === null) enableSearchBtn?.click();
     }
+
+    const vehicleTabs = document.querySelector('#tabs');
+    if (vehicleTabs) vehicleTabs.addEventListener('click', vehicleListChange);
+    //Initial expanding
+    vehicleListChange();
 };
