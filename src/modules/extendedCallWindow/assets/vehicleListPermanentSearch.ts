77126addca379f1b04f4b1aeec2a4a048a05a3f1
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2023-05-13 11:41:51 +0200

    ✨[ecw] add permanent search abr

---
+++ src/modules/extendedCallWindow/assets/vehicleListPermanentSearch.ts
@@ -0,0 +1,20 @@
+export default (): void => {
+    const vehicleTabs = document.querySelector('#tabs');
+    if (vehicleTabs) vehicleTabs.addEventListener('click', vehicleListChange);
+
+    /**
+     * Gets called on every vehicle tab change to update the vehicle search bar
+     * (for some reasons they have different search bars instead of filters).
+     */
+    function vehicleListChange() {
+        const vehicleSearch = document
+            .querySelector('#vehicle_list_step>.tab-content>.active')
+            ?.querySelector(
+                "[id^='mission_vehicle_search_outer_']"
+            ) as HTMLInputElement;
+        const enableSearchBtn = document
+            .querySelector('#vehicle_list_step>.tab-content>.active')
+            ?.querySelector("[id^='show_search_']") as HTMLAnchorElement;
+        if (vehicleSearch.offsetParent === null) enableSearchBtn.click();
+    }
+};
