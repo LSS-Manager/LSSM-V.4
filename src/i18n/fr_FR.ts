f3acf2d16fd23795bc360b3781f3b0a38b7dff3f
deoxis9001 <deoxis9001@users.noreply.github.com>
2023-05-21 11:00:45 +0200

    [fr_FR] Add list vehicles

--- src/i18n/fr_FR.ts
+++ src/i18n/fr_FR.ts
@@ -94,6 +94,9 @@ export default {
                 ['Ambulances']: [25, 26],
                 ['Nautique']: [16, 17],
                 ['Feux de Forêt']: [21, 22, 23, 24, 32, 33, 34, 35],
+                ['Aéroport']: [51,52,53,54,55],
+                ['bombardier d\'eau'] : [58,59,60]
+                ['Autres véhicules']:[56,57]
             },
             color: '#ff2d2d',
         },
