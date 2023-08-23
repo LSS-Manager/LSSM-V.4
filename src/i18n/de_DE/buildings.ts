9e48295194b2d0760bd6d593f60d56228c3a585d
Tobi-Continued <142994500+Tobi-Continued@users.noreply.github.com>
2023-08-23 21:03:11 +0200

    Update buildings.ts

--- src/i18n/de_DE/buildings.ts
+++ src/i18n/de_DE/buildings.ts
@@ -734,8 +734,8 @@ export default {
                 duration: '7 Tage',
                 isVehicleExtension: true,
                 givesParkingLots: 4,
-                unlocksVehicleTypes: [99, 100, 101, 102],
-                parkingLotReservations: [[99], [100], [101], [102]],
+                unlocksVehicleTypes: [99, 100, 101, 102, 123],
+                parkingLotReservations: [[99, 123], [100], [101], [102]],
                 requiredExtensions: [0, 1],
             },
             {
@@ -756,8 +756,8 @@ export default {
                 duration: '7 Tage',
                 isVehicleExtension: true,
                 givesParkingLots: 2,
-                unlocksVehicleTypes: [99, 112],
-                parkingLotReservations: [[99], [112]],
+                unlocksVehicleTypes: [99, 112, 122],
+                parkingLotReservations: [[99, 122], [112]],
                 requiredExtensions: [0, 1],
             },
             {
