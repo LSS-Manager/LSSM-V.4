82859bd1734469a1221986f5635f784d15662f4e
Tobias <Suchty112@users.noreply.github.com>
2023-09-22 06:35:30 +0200

    🚧[core] First 🛸Drone CP stuff

--- src/i18n/en_GB/equipment.ts
+++ src/i18n/en_GB/equipment.ts
@@ -1,3 +1,11 @@
 import registerEquipment from '../../registerEquipment';
 
-export default registerEquipment({});
+export default registerEquipment({
+    drone: {
+        id: 'drone',
+        caption: 'Drone',
+        size: 10,
+        credits: 25000,
+        coins: 15,
+    },
+});
