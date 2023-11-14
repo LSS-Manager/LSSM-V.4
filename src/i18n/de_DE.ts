1ebb5db8d7cb0767c7473427f78aa7adba46dd06
jan <jan.boehmerle@ledbrain.de>
2023-11-14 15:28:05 +0100

    🌐 [core] add new de_DE POI

--- src/i18n/de_DE.ts
+++ src/i18n/de_DE.ts
@@ -253,6 +253,7 @@ export default {
         'Moor',
         'Hüttenwerk',
         'Kraftwerk',
+        'Werksgelände',
     ],
     only_alliance_missions: [41, 43, 59, 75, 99, 207, 221, 222, 256, 350],
     transfer_missions: [147, 613],
