aecfe360a63a5062c263804ef2ea4050a41009db
Tobias <Suchty112@users.noreply.github.com>
2024-03-19 12:34:20 +0100

    ✨[core]🚒🚚 Hose trailer
    (cherry picked from commit 1c4dcf11be0968677a88c52146a4f20853fa9919)

--- src/i18n/de_DE/buildings.ts
+++ src/i18n/de_DE/buildings.ts
@@ -207,6 +207,21 @@ export default {
                 parkingLotReservations: [[138, 139, 140, 141], [138, 139, 140, 141]],
                 unlocksVehicleTypes: [138, 139, 140, 141, 142],
             },
+            ...multiplyExtension(
+                {
+                    caption: 'Anhänger-Stellplatz',
+                    credits: 75_000,
+                    coins: 15,
+                    duration: '3 Tage',
+                    isVehicleExtension: true,
+                    givesParkingLots: 1,
+                    unlocksVehicleTypes: [143],
+                    parkingLotReservations: [[143]],
+                    cannotDisable: true,
+                    unlockedVehiclesOnReservedLotsOnly: true,
+                },
+                5
+            ),
         ],
         storageUpgrades: {
             initial_containers: {
@@ -1475,6 +1490,21 @@ export default {
                 parkingLotReservations: [[138, 139, 140, 141],[138, 139, 140, 141]],
                 unlocksVehicleTypes: [138, 139, 140, 141, 142],
             },
+            ...multiplyExtension(
+                {
+                    caption: 'Anhänger-Stellplatz',
+                    credits: 75_000,
+                    coins: 15,
+                    duration: '3 Tage',
+                    isVehicleExtension: true,
+                    givesParkingLots: 1,
+                    unlocksVehicleTypes: [143],
+                    parkingLotReservations: [[143]],
+                    cannotDisable: true,
+                    unlockedVehiclesOnReservedLotsOnly: true,
+                },
+                2
+            ),
         ],
         storageUpgrades: {
             initial_containers: {
