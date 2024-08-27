ccf2582d908bbf514c7be4747390becfc3688289
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2024-08-27 09:44:09 +0200

    🐛️[all] even more fixing

--- src/modules/enhancedPOI/main.ts
+++ src/modules/enhancedPOI/main.ts
@@ -140,7 +140,7 @@ export default (async ({ LSSM, MODULE_ID, $m, getSetting, setSetting }) => {
             'poi-hider-style',
             true
         );
-        document.getElementById(`${extraStyleId}`)?.remove();
+        document.getElementById(extraStyleId)?.remove();
         const style = document.createElement('style');
         style.id = extraStyleId;
         style.textContent = `${selector} {display: block !important;}`;
@@ -168,9 +168,7 @@ export default (async ({ LSSM, MODULE_ID, $m, getSetting, setSetting }) => {
             }
             if (
                 isPOIWindow &&
-                document.querySelector<HTMLDivElement>(
-                    `#${poiSettingsWrapperId}`
-                )
+                document.getElementById(poiSettingsWrapperId) as HTMLDivElement
             )
                 return;
             isPOIWindow = true;
