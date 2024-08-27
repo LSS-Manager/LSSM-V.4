3828c8006ca32489c3fbd5ddbdc1f245bbfbc8d0
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2024-08-27 11:51:29 +0200

    🚧[lssmaql] get buildings&vehicles to work

--- src/modules/lssmaql/lssmaql.vue
+++ src/modules/lssmaql/lssmaql.vue
@@ -1,32 +1,32 @@
 <template>
-    <lightbox name="lssmaql" no-title-hide>
+    <lightbox name='lssmaql' no-title-hide>
         <h1>
-            <font-awesome-icon :icon="faTerminal"></font-awesome-icon>
+            <font-awesome-icon :icon='faTerminal'></font-awesome-icon>
             LSSMAQL Console
         </h1>
-        <form @submit="getResult">
-            <div class="input-group">
+        <form @submit='getResult'>
+            <div class='input-group'>
                 <label>
                     your LSSMAQL Query
                     <input
-                        type="text"
-                        class="form-control"
-                        v-model="query"
-                        @keydown.enter="getResult"
+                        type='text'
+                        class='form-control'
+                        v-model='query'
+                        @keydown.enter='getResult'
                     />
                 </label>
             </div>
-            <a class="btn btn-success" @click="getResult"> Execute </a>
+            <a class='btn btn-success' @click='getResult'> Execute </a>
         </form>
-        <div class="row">
-            <div class="col-sm-8">
+        <div class='row'>
+            <div class='col-sm-8'>
                 <b>Result ({{ resultLength }}):</b>
                 <pre>{{ resultString }}</pre>
             </div>
         </div>
     </lightbox>
 </template>
-<script lang="ts">
+<script lang='ts'>
 import { computed, ref } from 'vue';
 
 import jsonata from 'jsonata';
@@ -38,7 +38,7 @@ export default {
         Lightbox: () =>
             import(
                 /* webpackChunkName: "components/lightbox" */ '../../components/LightboxWrapper.vue'
-            ),
+                ),
     },
     setup() {
         const query = ref('');
@@ -58,7 +58,9 @@ export default {
         async function getResult(): Promise<void> {
             const expression = jsonata(query.value);
             resultString.value = JSON.stringify(
-                await expression.evaluate(getAPIData())
+                await expression.evaluate(getAPIData()),
+                null,
+                2,
             );
         }
 
@@ -68,13 +70,13 @@ export default {
          */
         function getAPIData() {
             const apiStore = useAPIStore();
-            apiStore.getBuildings('lssmaql-beforeMount');
-            apiStore.getVehicles('lssmaql-beforeMount');
+            apiStore.getBuildings('lssmaql-beforeMount', true);
+            apiStore.getVehicles('lssmaql-beforeMount', true);
             apiStore.getMissionTypesArray('lssmaql-beforeMount');
             return {
                 missions: apiStore.getMissionTypesArray('lssmaql'),
-                buildings: apiStore.buildings,
-                vehicles: apiStore.buildings,
+                buildings: apiStore.buildingsArray,
+                vehicles: apiStore.vehiclesArray,
             };
         }
 
@@ -82,14 +84,14 @@ export default {
     },
     beforeMount() {
         const apiStore = useAPIStore();
+        apiStore.getBuildings('lssmaql-beforeMount', true);
+        apiStore.getVehicles('lssmaql-beforeMount', true);
         apiStore.getMissionTypesArray('lssmaql-beforeMount');
-        apiStore.getBuildings('lssmaql-beforeMount');
-        apiStore.getVehicles('lssmaql-beforeMount');
     },
 };
 </script>
 
-<style scoped lang="sass">
+<style scoped lang='sass'>
 form :not(.btn),
 .row
     width: 100%
