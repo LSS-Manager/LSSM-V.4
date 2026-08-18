c3da994c57eeee56cbfbb9bf5b558b829a5b8553
A951951 <bh8729@hotmail.com>
2026-08-18 08:58:53 -0400

    Update local userscript for MissionChief domain change

---
+++ static/lssm-v4.local.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         LSS-Manager V.4 – Local
+// @version      4.7.12-CZ-DK-DE-AU-GB-US-ES-MX-FI-FR-IT-JP-KR-NO-NL-PL-BR-PT-RO-RU-SK-SE-TR-UA
+// @author       The LSSM-Team: Sanni, Jan (jxn_30), Ron31, Crazycake
+// @description  LSSM V4 is a Script-Collection for Leitstellenspiel, Missionchief, Meldkamerspel and its other language versions.
+// @namespace    https://lss-manager.de/
+// @homepage     https://localhost:36551/docs/
+// @downloadURL  https://localhost:36551/lssm-v4.user.js
+// @updateURL    https://localhost:36551/lssm-v4.user.js
+// @supportURL   https://localhost:36551/docs/en_US/error_report
+// @icon         https://localhost:36551/docs/img/lssm.png
+// @match        https://www.operacni-stredisko.cz/*
+// @match        https://policie.operacni-stredisko.cz/*
+// @match        https://www.alarmcentral-spil.dk/*
+// @match        https://politi.alarmcentral-spil.dk/*
+// @match        https://www.leitstellenspiel.de/*
+// @match        https://polizei.leitstellenspiel.de/*
+// @match        https://www.missionchief-australia.com/*
+// @match        https://police.missionchief-australia.com/*
+// @match        https://www.missionchief.co.uk/*
+// @match        https://police.missionchief.co.uk/*
+// @match        https://www.missionchief.com/*
+// @match        https://missionchief.com/*
+// @match        https://police.missionchief.com/*
+// @match        https://www.centro-de-mando.es/*
+// @match        https://www.centro-de-mando.mx/*
+// @match        https://www.hatakeskuspeli.com/*
+// @match        https://poliisi.hatakeskuspeli.com/*
+// @match        https://www.operateur112.fr/*
+// @match        https://police.operateur112.fr/*
+// @match        https://www.operatore112.it/*
+// @match        https://polizia.operatore112.it/*
+// @match        https://www.missionchief-japan.com/*
+// @match        https://www.missionchief-korea.com/*
+// @match        https://www.nodsentralspillet.com/*
+// @match        https://politiet.nodsentralspillet.com/*
+// @match        https://www.meldkamerspel.com/*
+// @match        https://politie.meldkamerspel.com/*
+// @match        https://www.operatorratunkowy.pl/*
+// @match        https://policja.operatorratunkowy.pl/*
+// @match        https://www.operador193.com/*
+// @match        https://www.jogo-operador112.com/*
+// @match        https://policia.jogo-operador112.com/*
+// @match        https://www.jocdispecerat112.com/*
+// @match        https://www.dispetcher-112.com/*
+// @match        https://www.dispecerske-centrum.com/*
+// @match        https://www.larmcentralen-spelet.se/*
+// @match        https://polis.larmcentralen-spelet.se/*
+// @match        https://www.112-merkez.com/*
+// @match        https://www.dyspetcher101-game.com/*
+// @run-at       document-idle
+// @grant        GM_info
+// @grant        unsafeWindow
+// @grant        GM_getResourceURL
+// @resource     core https://localhost:36551/core.js
+// ==/UserScript==
+/* global I18n, user_id */
+const e=()=>{const e=document.createElement("script");e.src=GM_getResourceURL("core");e.setAttribute("type","module");e.setAttribute("async","");unsafeWindow["lssmv4-GM_Info"]=JSON.parse(JSON.stringify(GM_info));document.body.append(e)};if((!window.frameElement||window.frameElement?.src.startsWith("https"))&&!/^\/users\//u.test(window.location.pathname)&&"undefined"!=typeof user_id&&"undefined"!=typeof I18n)if(window!==window.parent&&window.parent.hasOwnProperty("lssmv4-redesign-lightbox")){const n="lssmv4-redesign-iframe-trigger-lssm-load";window.parent.addEventListener(n,e);window.addEventListener("pagehide",()=>window.parent.removeEventListener(n,e));window.parent["lssmv4-redesign-lightbox"].src=new URL(window.location.href).toString()}else e();
